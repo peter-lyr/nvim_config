944d1686b495f0ffa3a3e650dedfd6b8524c61e8
peter-lyr <llydrp.ldp@gmail.com>
2023-06-13 00:19:05 +0800

    2000ms后才允许执行DiffviewClose

--- start/lazy/lua/config/diffview.lua
+++ start/lazy/lua/config/diffview.lua
@@ -1,8 +1,51 @@
 require("diffview").setup()
 
-vim.keymap.set({ 'n', 'v' }, '<leader>gi', ':<c-u>DiffviewFileHistory<cr>', { silent = true, desc = 'diffview filehistory' })
-vim.keymap.set({ 'n', 'v' }, '<leader>go', ':<c-u>DiffviewOpen -u<cr>', { silent = true, desc = 'diffview open' })
-vim.keymap.set({ 'n', 'v' }, '<leader>gq', ':<c-u>DiffviewClose<cr>', { silent = true, desc = 'diffview quit' })
+local close_allow_timeout = 0
+local close_allow_timer
+local close_allowed = nil
+
+local function close_allow_timer_do()
+  close_allowed = nil
+  close_allow_timeout = 2000
+  if close_allow_timer then
+    close_allow_timer:stop()
+  end
+  close_allow_timer = vim.loop.new_timer()
+  if close_allow_timer then
+    close_allow_timer:start(200, 200, function()
+      vim.schedule(function()
+        close_allow_timeout = close_allow_timeout - 200
+        if close_allow_timeout <= 0 then
+          close_allow_timer:stop()
+          close_allow_timer = nil
+          close_allowed = 1
+        end
+      end)
+    end)
+  end
+end
+
+local function diffviewfilehistory()
+  vim.cmd('DiffviewFileHistory')
+  close_allow_timer_do()
+end
+
+local function diffviewopen()
+  vim.cmd('DiffviewOpen -u')
+  close_allow_timer_do()
+end
+
+local function diffviewclose()
+  if close_allowed then
+    vim.cmd('DiffviewClose')
+  else
+    print(close_allow_timeout .. 'ms后再尝试')
+  end
+end
+
+vim.keymap.set({ 'n', 'v' }, '<leader>gi', diffviewfilehistory, { silent = true, desc = 'diffview filehistory' })
+vim.keymap.set({ 'n', 'v' }, '<leader>go', diffviewopen, { silent = true, desc = 'diffview open' })
+vim.keymap.set({ 'n', 'v' }, '<leader>gq', diffviewclose, { silent = true, desc = 'diffview close' })
 
 vim.keymap.set({ 'n', 'v' }, '<leader>ge', ':<c-u>DiffviewRefresh<cr>', { silent = true, desc = 'DiffviewRefresh' })
 vim.keymap.set({ 'n', 'v' }, '<leader>gl', ':<c-u>DiffviewToggleFiles<cr>', { silent = true, desc = 'DiffviewToggleFiles' })
