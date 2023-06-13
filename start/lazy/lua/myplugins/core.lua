933b8d9b62a1cfaf67e8ce981e61cad2df8189b5
peter-lyr <llydrp.ldp@gmail.com>
2023-06-13 20:02:07 +0800

    map lazy event keys

--- start/lazy/lua/myplugins/core.lua
+++ start/lazy/lua/myplugins/core.lua
@@ -7,6 +7,36 @@ return {
   },
   {
     name = 'maps',
+    lazy = true,
     dir = opt .. 'maps',
+    event = { 'CmdlineEnter', 'InsertEnter', 'ModeChanged', },
+    keys = {
+      'c.',
+      'cu',
+      'c-',
+
+      '<a-y>',
+      '<a-p>',
+      '<a-s-p>',
+
+      '<leader>y',
+      '<leader>gy',
+      '<leader><leader>gy',
+
+      '<c-j>',
+      '<c-k>',
+
+      '<f5>',
+
+      '<rightmouse>',
+      '<rightrelease>',
+      '<middlemouse>',
+
+      'Q',
+
+      '<leader>f.',
+
+      'U',
+    },
   },
 }
