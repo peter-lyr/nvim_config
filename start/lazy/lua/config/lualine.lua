3536b85d37c9a3ce0e4634d7938301822a87b4b4
peter-lyr <llydrp.ldp@gmail.com>
2023-06-17 20:52:25 +0800

    lualine

--- start/lazy/lua/config/lualine.lua
+++ start/lazy/lua/config/lualine.lua
@@ -16,24 +16,61 @@ require('lualine').setup({
   sections = {
     lualine_c = {
       'filesize',
-      { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-      { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+      {
+        "filetype",
+        icon_only = true,
+        separator = "",
+        padding = {
+          left = 1,
+          right = 0,
+        }
+      },
+      {
+        "filename",
+        path = 1,
+        symbols = {
+          modified = "  ",
+          readonly = "",
+          unnamed = "",
+        },
+      },
     },
     lualine_x = {
       {
-        function() return require("noice").api.status.command.get() end,
-        cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+        function()
+          return require("noice").api.status.command.get()
+        end,
+        cond = function()
+          return package.loaded["noice"] and require("noice").api.status.command.has()
+        end,
       },
       {
-        function() return '@' .. vim.fn.reg_recording() end,
-        cond = function() return #vim.fn.reg_recording() > 0 end,
+        function()
+          return '@' .. vim.fn.reg_recording()
+        end,
+        cond = function()
+          return #vim.fn.reg_recording() > 0
+        end,
       },
     },
     lualine_y = {
       'encoding',
       'fileformat',
-      { "progress", separator = " ", padding = { left = 1, right = 0 } },
-      { "location", padding = { left = 0, right = 1 } },
+      {
+        "progress",
+        separator = " ",
+        padding = {
+          left = 1,
+          right = 0,
+        },
+      },
+      {
+        "location",
+        padding = {
+          left = 0,
+          right = 1,
+        },
+      },
     },
     lualine_z = {
       function()
@@ -43,10 +80,16 @@ require('lualine').setup({
   },
   inactive_sections = {
     lualine_c = {
-      { "filename", path = 1, },
+      {
+        "filename",
+        path = 1,
+      },
     },
     lualine_x = {
-      { "filetype", icons_enabled = false },
+      {
+        "filetype",
+        icons_enabled = false,
+      },
     },
   },
 })
