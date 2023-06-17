60d4f5d45e30a52ca3408d38067e734a8fcb7115
peter-lyr <llydrp.ldp@gmail.com>
2023-06-17 20:10:25 +0800

    lualine

--- start/lazy/lua/config/lualine.lua
+++ start/lazy/lua/config/lualine.lua
@@ -1,1 +1,54 @@
-require('lualine').setup()
+require('lualine').setup({
+  options = {
+    component_separators = {
+      left = "",
+      right = ""
+    },
+    section_separators = {
+      left = "",
+      right = ""
+    },
+    globalstatus = true,
+  },
+  sections = {
+    lualine_c = {
+      'filesize',
+      {
+        "filetype",
+        icon_only = true,
+        separator = "",
+        padding = {
+          left = 1,
+          right = 0
+        }
+      },
+      { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+    },
+    lualine_x = {
+      {
+        function() return require("noice").api.status.command.get() end,
+        cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+      },
+      {
+        function() return '@' .. vim.fn.reg_recording() end,
+        cond = function() return #vim.fn.reg_recording() > 0 end,
+      },
+    },
+    lualine_y = {
+      'encoding',
+      'fileformat',
+      { "progress", separator = " ", padding = { left = 1, right = 0 } },
+      { "location", padding = { left = 0, right = 1 } },
+    },
+    lualine_z = {
+      function()
+        return " " .. os.date("%R")
+      end,
+    },
+  },
+  inactive_sections = {
+    lualine_c = {
+      { "filename", path = 1, },
+    },
+  },
+})
--- start/lazy/lua/plugins/lualine.lua
+++ start/lazy/lua/plugins/lualine.lua
@@ -3,6 +3,15 @@ return {
   event = "VeryLazy",
   dependencies = {
     require('plugins.web-devicons'),
+    {
+      "folke/noice.nvim",
+      opts = {
+      },
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+      },
+    }
   },
   config = function()
     require('config.lualine')
