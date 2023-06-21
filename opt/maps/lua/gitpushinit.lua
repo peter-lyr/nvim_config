ed4fa254c6b46c24279c9e06aa4d7c250e1feab2
peter-lyr <llydrp.ldp@gmail.com>
2023-06-22 03:13:53 +0800

    ser

--- opt/maps/lua/gitpushinit.lua
+++ opt/maps/lua/gitpushinit.lua
@@ -3,16 +3,21 @@ local M = {}
 M.push = function()
   local result = vim.fn.systemlist({ "git", "status", "-s" })
   if #result > 0 then
-    require('notify').notify(vim.loop.cwd() .. '\n' .. table.concat(result))
-    local input = vim.fn.input('commit info (Add all and push): ')
-    if #input > 0 then
-      vim.loop.new_timer():start(10, 0, function()
-        vim.schedule(function()
-          vim.cmd([[au User AsyncRunStop lua local l = vim.fn.getqflist(); vim.notify(l[#l]['text']); vim.cmd('au! User AsyncRunStop')]])
-          vim.cmd(string.format('AsyncRun git add -A && git status && git commit -m "%s" && git push', input))
-        end)
-      end)
-    end
+    require('notify').notify(vim.loop.cwd() .. '\n' .. table.concat(result), 2, {
+      keep = function() 
+        local input = vim.fn.input('commit info (Add all and push): ')
+        if #input > 0 then
+          vim.loop.new_timer():start(10, 0, function()
+            vim.schedule(function()
+              vim.cmd([[au User AsyncRunStop lua local l = vim.fn.getqflist(); vim.notify(l[#l]['text']); vim.cmd('au! User AsyncRunStop')]])
+              vim.cmd(string.format('AsyncRun git add -A && git status && git commit -m "%s" && git push', input))
+            end)
+          end)
+        end
+      end,
+      animate = false,
+      timeout = 10,
+    })
   end
 end
 
