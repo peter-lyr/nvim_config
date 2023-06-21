f6f08c9d9716a9992a65932e8ef74370e4e3d8c6
peter-lyr <llydrp.ldp@gmail.com>
2023-06-21 22:06:14 +0800

    GuiFont!

--- opt/maps/lua/fontsize.lua
+++ opt/maps/lua/fontsize.lua
@@ -15,8 +15,8 @@ M.sizeup = function()
   local fontname, fontsize = getfontnamesize()
   fontsize = fontsize + 1
   if fontsize <= 72 then
-    vim.fn['GuiFont'](fontname .. fontsize)
-    print(fontname .. fontsize)
+    vim.cmd('GuiFont! ' .. fontname .. fontsize)
+    print('GuiFont! ' .. fontname .. fontsize)
   end
 end
 
@@ -24,21 +24,21 @@ M.sizedown = function()
   local fontname, fontsize = getfontnamesize()
   fontsize = fontsize - 1
   if fontsize >= 1 then
-    vim.fn['GuiFont'](fontname .. fontsize)
-    print(fontname .. fontsize)
+    vim.cmd('GuiFont! ' .. fontname .. fontsize)
+    print('GuiFont! ' .. fontname .. fontsize)
   end
 end
 
 M.sizenormal = function()
   local fontname, _ = getfontnamesize()
-  vim.fn['GuiFont'](fontname .. M.fontsizenormal)
-  print(fontname .. M.fontsizenormal)
+  vim.cmd('GuiFont! ' .. fontname .. M.fontsizenormal)
+  print('GuiFont! ' .. fontname .. M.fontsizenormal)
 end
 
 M.sizemin = function()
   local fontname, _ = getfontnamesize()
-  vim.fn['GuiFont'](fontname .. 1)
-  print(fontname .. 1)
+  vim.cmd('GuiFont! ' .. fontname .. 1)
+  print('GuiFont! ' .. fontname .. 1)
 end
 
 return M
