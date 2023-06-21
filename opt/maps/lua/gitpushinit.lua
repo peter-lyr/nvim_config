6ecbb2a202e5f31d5bb95b3666a7ea0692b92396
peter-lyr <llydrp.ldp@gmail.com>
2023-06-22 02:54:13 +0800

    test gitpushinit push

--- opt/maps/lua/gitpushinit.lua
+++ opt/maps/lua/gitpushinit.lua
@@ -2,7 +2,18 @@ local M = {}
 
 M.push = function()
   local result = vim.fn.systemlist({ "git", "status", "-s" })
-  print(vim.inspect(result), '===')
+  if #result > 0 then
+    require('notify').notify(vim.loop.cwd() .. '\n' .. table.concat(result))
+    local input = vim.fn.input('commit info (Add all and push): ')
+    if #input > 0 and #vim.fn.input('Sure to add all and push? (Empty for yes): ') == 0 then
+      vim.loop.new_timer():start(10, 0, function()
+        vim.schedule(function()
+          vim.cmd([[au User AsyncRunStop lua local l = vim.fn.getqflist(); vim.notify(l[#l]['text']); vim.cmd('au! User AsyncRunStop')]])
+          vim.cmd(string.format('AsyncRun git add -A && git status && git commit -m "%s" && git push', input))
+        end)
+      end)
+    end
+  end
 end
 
 M.init = function()
