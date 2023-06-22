10db446bd64b5df45f0c1bec50cd625eb622be77
peter-lyr <llydrp.ldp@gmail.com>
2023-06-23 02:02:48 +0800

    indentscope lazyvim

--- start/lazy/lua/plugins/intendscope.lua
+++ start/lazy/lua/plugins/intendscope.lua
@@ -1,10 +1,30 @@
 return {
   'echasnovski/mini.indentscope',
   lazy = true,
-  event = { 'CursorMoved', },
+  event = { "BufReadPre", "BufNewFile" },
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "help",
+        -- "alpha",
+        -- "dashboard",
+        "neo-tree",
+        -- "Trouble",
+        "lazy",
+        "mason",
+        "notify",
+        -- "toggleterm",
+        -- "lazyterm",
+      },
+      callback = function()
+        vim.b.miniindentscope_disable = true
+      end,
+    })
+  end,
   config = function()
     require('mini.indentscope').setup({
-      symbol = '┃',
+      symbol = "│",
+      options = { try_as_border = true },
     })
   end,
 }
