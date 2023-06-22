2f640c93c07994fa9e9bdec598e7eaa4f7c7362b
peter-lyr <llydrp.ldp@gmail.com>
2023-06-22 17:16:39 +0800

    lualine buffers show help

--- start/lazy/lua/config/lualine.lua
+++ start/lazy/lua/config/lualine.lua
@@ -175,8 +175,8 @@ require('lualine').setup({
         use_mode_colors = true,
         show_buffers = function()
           local buffers = {}
-          for b = 1, vim.fn.bufnr('$') do
-            if vim.fn.buflisted(b) ~= 0 and vim.api.nvim_buf_get_option(b, 'buftype') ~= 'quickfix' then
+          for _, b in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.fn.buflisted(b) ~= 0 and vim.api.nvim_buf_get_option(b, 'buftype') ~= 'quickfix' or vim.api.nvim_buf_get_option(b, 'buftype') == 'help' then
               local fname = vim.api.nvim_buf_get_name(b)
               if #projectroot == 0 or #fname > 0 and vim.fn['ProjectRootGet'](fname) == projectroot then
                 buffers[#buffers+1] = b
