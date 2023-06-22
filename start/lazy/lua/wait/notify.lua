1c60776a61847ac9513b335e04cf52cdb0d54074
peter-lyr <llydrp.ldp@gmail.com>
2023-06-23 02:30:30 +0800

    notify max_height

--- start/lazy/lua/wait/notify.lua
+++ start/lazy/lua/wait/notify.lua
@@ -7,6 +7,13 @@ return {
   config = function()
     require("notify").setup({
       top_down = false,
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
     })
   end,
 }
