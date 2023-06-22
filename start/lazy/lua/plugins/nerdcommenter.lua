c0efa7a11ce9d9319ed8e5eaa7e6f76afe5c5fa4
peter-lyr <llydrp.ldp@gmail.com>
2023-06-22 14:33:32 +0800

    nerdcommenter

---
+++ start/lazy/lua/plugins/nerdcommenter.lua
@@ -0,0 +1,27 @@
+return {
+  'preservim/nerdcommenter',
+  lazy = true,
+  init = function()
+    vim.g.NERDSpaceDelims = 1
+    vim.g.NERDDefaultAlign = "left"
+    vim.g.NERDCommentEmptyLines = 1
+    vim.g.NERDTrimTrailingWhitespace = 1
+    vim.g.NERDToggleCheckAllLines = 1
+    vim.g.NERDCustomDelimiters = {
+      markdown = {
+        left = '<!--', right = '-->',
+        leftAlt = '[', rightAlt = ']: #',
+      },
+      c = {
+        left = '//', right = '',
+        leftAlt = '/*', rightAlt = '*/',
+      },
+    }
+  end,
+  keys = {
+    { '<leader>cp', "vip:call nerdcommenter#Comment('x', 'toggle')<CR>", mode = { 'n', }, desc = 'comment a paragraph' },
+    { '<leader>c}', "V}k:call nerdcommenter#Comment('x', 'toggle')<CR>", mode = { 'n', }, desc = 'comment paragraph till end' },
+    { '<leader>c{', "V{j:call nerdcommenter#Comment('x', 'toggle')<CR>", mode = { 'n', }, desc = 'comment paragraph till start' },
+    { '<leader>cG', "VG:c nerdcommenter#Comment('x', 'toggle')<CR>",     mode = { 'n', }, desc = 'comment till end of line' },
+  },
+}
