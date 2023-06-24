8a93b02616feb49315d0a1f12af644766dc13ef2
peter-lyr <llydrp.ldp@gmail.com>
2023-06-24 15:01:23 +0800

    GuiWindowFrameless make StartupTime error

--- start/lazy/plugin/winenter.lua
+++ start/lazy/plugin/winenter.lua
@@ -1,1 +1,9 @@
-vim.fn['GuiWindowFrameless'](1)
+vim.api.nvim_create_autocmd({ "VimEnter", }, {
+  callback = function()
+    vim.loop.new_timer():start(10, 0, function()
+      vim.schedule(function()
+        vim.fn['GuiWindowFrameless'](1)
+      end)
+    end)
+  end,
+})
