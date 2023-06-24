05b987887659d2f343037f66d8f6b6c87adb57be
peter-lyr <llydrp.ldp@gmail.com>
2023-06-24 20:49:42 +0800

    pack/localappdata/project_nvim

--- start/lazy/lua/config/telescope_projects.lua
+++ start/lazy/lua/config/telescope_projects.lua
@@ -1,18 +1,11 @@
 local M = {}
 
-local datapath = require("plenary.path"):new(vim.fn.expand("$VIMRUNTIME")):joinpath("pack", "data")
-
-if not datapath:exists() then
-  vim.fn.mkdir(datapath.filename)
-end
-
 local patterns = {
   ".git",
 }
 
 require("project_nvim").setup({
   manual_mode = false,
-  datapath = datapath.filename,
   detection_methods = { "pattern", "lsp" },
   patterns = patterns,
 })
