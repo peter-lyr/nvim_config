929a30192ada73f86683fc51429da109c0ff5516
peter-lyr <llydrp.ldp@gmail.com>
2023-06-26 20:38:19 +0800

    v0.10.0 only nvim.exe

--- create-exe.bat
+++ create-exe.bat
@@ -19,20 +19,29 @@ cd %pack%nvim_config
 REM  try kill exe
 
 taskkill /f /im start-nvim-qt.exe
+taskkill /f /im start-nvim.exe
 
-REM  build exe
+REM  build nvim-qt.exe
 
 windres -i "neovim.ico.rc" -o "neovim.ico.o"
 gcc start-nvim-qt.c neovim.ico.o -Wall -s -ffunction-sections -fdata-sections -Wl,--gc-sections -O3 -o start-nvim-qt
+
+REM  build nvim.exe
+
+gcc start-nvim.c -Wall -s -ffunction-sections -fdata-sections -Wl,--gc-sections -O3 -o start-nvim
 
 REM  compress exe
 
 strip -s %pack%nvim_config\start-nvim-qt.exe
 upx --best %pack%nvim_config\start-nvim-qt.exe
 
+strip -s %pack%nvim_config\start-nvim.exe
+upx --best %pack%nvim_config\start-nvim.exe
+
 REM  run exe
 
 REM  %pack%nvim_config\start-nvim-qt
+REM  %pack%nvim_config\start-nvim
 
 REM  exit bat
 
--- start/lazy/plugin/winenter.lua
+++ start/lazy/plugin/winenter.lua
@@ -1,9 +1,9 @@
-vim.api.nvim_create_autocmd({ "VimEnter", }, {
-  callback = function()
-    vim.loop.new_timer():start(10, 0, function()
-      vim.schedule(function()
-        vim.fn['GuiWindowFrameless'](1)
-      end)
-    end)
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "VimEnter", }, {
+--   callback = function()
+--     vim.loop.new_timer():start(10, 0, function()
+--       vim.schedule(function()
+--         vim.fn['GuiWindowFrameless'](1)
+--       end)
+--     end)
+--   end,
+-- })
