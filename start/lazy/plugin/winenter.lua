ad3b0f283dd40dff56b51de7838ef02845a2e3ff
peter-lyr <llydrp.ldp@gmail.com>
2023-06-27 00:10:44 +0800

    del nvim-qt stuff

--- start/lazy/plugin/winenter.lua
+++
@@ -1,9 +0,0 @@
--- vim.api.nvim_create_autocmd({ "VimEnter", }, {
---   callback = function()
---     vim.loop.new_timer():start(10, 0, function()
---       vim.schedule(function()
---         vim.fn['GuiWindowFrameless'](1)
---       end)
---     end)
---   end,
--- })
