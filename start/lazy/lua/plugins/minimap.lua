86436d8ffdd5aa21c98a77777f6cc2b48a180626
peter-lyr <llydrp.ldp@gmail.com>
2023-07-01 05:26:57 +0800

    MinimapRescan

--- start/lazy/lua/plugins/minimap.lua
+++ start/lazy/lua/plugins/minimap.lua
@@ -11,6 +11,7 @@ return {
           local bufnr = vim.fn.bufnr('-MINIMAP-')
           local winnr = vim.fn.bufwinnr('-MINIMAP-')
           if winnr ~= -1 then
+            vim.cmd('MinimapRescan')
             vim.fn.win_gotoid(vim.fn.win_getid(winnr))
             vim.keymap.set({ 'n', 'v', }, 'q', '<cmd>wincmd p<cr>', { desc = 'enter', buffer = bufnr })
           end
