7a7b07cd8133c52f99755cc4764b055a191a045e
peter-lyr <llydrp.ldp@gmail.com>
2023-07-02 13:16:21 +0800

    check code-minimap.exe

--- start/lazy/lua/plugins/minimap.lua
+++ start/lazy/lua/plugins/minimap.lua
@@ -7,17 +7,19 @@ return {
     {
       '<leader>4',
       function()
-        if vim.fn.filereadable(vim.api.nvim_buf_get_name(0)) == 1 then
-          vim.cmd('Minimap')
-          vim.loop.new_timer():start(50, 0, function()
-            vim.schedule(function()
-              local winnr = vim.fn.bufwinnr('-MINIMAP-')
-              if winnr ~= -1 then
-                pcall(vim.cmd, 'MinimapRescan')
-                pcall(vim.fn.win_gotoid, vim.fn.win_getid(winnr))
-              end
+        if vim.g.loaded_minimap then
+          if vim.fn.filereadable(vim.api.nvim_buf_get_name(0)) == 1 then
+            vim.cmd('Minimap')
+            vim.loop.new_timer():start(50, 0, function()
+              vim.schedule(function()
+                local winnr = vim.fn.bufwinnr('-MINIMAP-')
+                if winnr ~= -1 then
+                  pcall(vim.cmd, 'MinimapRescan')
+                  pcall(vim.fn.win_gotoid, vim.fn.win_getid(winnr))
+                end
+              end)
             end)
-          end)
+          end
         end
       end,
       mode = { 'n', 'v' },
@@ -32,30 +34,32 @@ return {
     vim.g.minimap_block_filetypes = { 'aerial', 'neo-tree', 'help', }
   end,
   config = function()
-    vim.api.nvim_create_autocmd({ "WinClosed", }, {
-      callback = function()
-        if vim.fn.bufnr() == vim.fn.bufnr('-MINIMAP-') then
-          if vim.fn.expand('<afile>') == tostring(vim.fn.win_getid()) then
-            vim.cmd('MinimapClose')
+    if vim.g.loaded_minimap then
+      vim.api.nvim_create_autocmd({ "WinClosed", }, {
+        callback = function()
+          if vim.fn.bufnr() == vim.fn.bufnr('-MINIMAP-') then
+            if vim.fn.expand('<afile>') == tostring(vim.fn.win_getid()) then
+              vim.cmd('MinimapClose')
+            end
           end
-        end
-      end,
-    })
-    vim.api.nvim_create_autocmd({ "BufEnter", }, {
-      callback = function()
-        if vim.fn.filereadable(vim.api.nvim_buf_get_name(0)) == 1 then
-          vim.loop.new_timer():start(10, 0, function()
-            vim.schedule(function()
-              vim.cmd('Minimap')
+        end,
+      })
+      vim.api.nvim_create_autocmd({ "BufEnter", }, {
+        callback = function()
+          if vim.fn.filereadable(vim.api.nvim_buf_get_name(0)) == 1 then
+            vim.loop.new_timer():start(10, 0, function()
+              vim.schedule(function()
+                vim.cmd('Minimap')
+              end)
             end)
-          end)
-        end
-      end,
-    })
-    vim.api.nvim_create_autocmd({ "TabLeave", }, {
-      callback = function()
-        vim.cmd('MinimapClose')
-      end,
-    })
+          end
+        end,
+      })
+      vim.api.nvim_create_autocmd({ "TabLeave", }, {
+        callback = function()
+          vim.cmd('MinimapClose')
+        end,
+      })
+    end
   end
 }
