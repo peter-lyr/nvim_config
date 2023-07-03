1dd5bc139831d00af9731967b4b4645f8a4c37bb
peter-lyr <llydrp.ldp@gmail.com>
2023-07-03 16:59:03 +0800

    proxy toggle

--- opt/computer/lua/Windows.lua
+++ opt/computer/lua/Windows.lua
@@ -12,4 +12,16 @@ M.monitor_30min = function()
   print(cmd)
 end
 
+M.proxy_on = function()
+  local cmd = [[!reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f]]
+  vim.cmd(cmd)
+  print('proxy on')
+end
+
+M.proxy_off = function()
+  local cmd = [[!reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f]]
+  vim.cmd(cmd)
+  print('proxy off')
+end
+
 return M
--- start/lazy/lua/myplugins/computer.lua
+++ start/lazy/lua/myplugins/computer.lua
@@ -13,8 +13,13 @@ return {
 
       -- 关闭显示器倒计时
 
-      { '<F6>A', function() require('Windows').monitor_1min() end, mode = { 'n', 'v' },  silent = true, desc = 'monitor close timeout' },
-      { '<F6>a', function() require('Windows').monitor_30min() end, mode = { 'n', 'v' }, silent = true, desc = 'monitor close timeout' },
+      { '<F6>A', function() require('Windows').monitor_1min() end, mode = { 'n', 'v' },  silent = true, desc = 'monitor close timeout 1min' },
+      { '<F6>a', function() require('Windows').monitor_30min() end, mode = { 'n', 'v' }, silent = true, desc = 'monitor close timeout 30min' },
+
+      -- 系统代理
+
+      { '<F6>b', function() require('Windows').proxy_on() end, mode = { 'n', 'v' },  silent = true, desc = 'proxy_on' },
+      { '<F6>B', function() require('Windows').proxy_off() end, mode = { 'n', 'v' }, silent = true, desc = 'proxy_off' },
 
     },
   },
