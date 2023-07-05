636c0eba69529c893b229e8555b7ec041eee6018
peter-lyr <llydrp.ldp@gmail.com>
2023-07-06 01:23:32 +0800

    spectre nmap

--- start/lazy/lua/plugins/spectre.lua
+++ start/lazy/lua/plugins/spectre.lua
@@ -5,7 +5,7 @@ return {
     { '<leader>re', function() require("spectre").open() end,                                   mode = { 'n', 'v', }, silent = true, desc = 'Open Spectre' },
     { '<leader>rs', function() require("spectre").open_visual({ select_word = true }) end,      mode = { 'n', },      silent = true, desc = 'Search current word' },
     { '<leader>rs', function() require("spectre").open_visual() end,                            mode = { 'v', },      silent = true, desc = 'Search current word' },
-    { '<leader>rp', function() require("spectre").open_file_search({ select_word = true }) end, mode = { 'v', },      silent = true, desc = 'Search on current file' },
+    { '<leader>rp', function() require("spectre").open_file_search({ select_word = true }) end, mode = { 'n', 'v', }, silent = true, desc = 'Search on current file' },
   },
   config = function()
     require('spectre').setup()
