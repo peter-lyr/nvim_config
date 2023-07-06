822fa4279460e9574fdfea061a58475fda8cd49a
peter-lyr <llydrp.ldp@gmail.com>
2023-07-07 01:16:23 +0800

    note mkd export

---
+++ opt/note/lua/mkdexport.lua
@@ -0,0 +1,43 @@
+local M = {}
+
+vim.g.mkdexportpy = require("plenary.path"):new(vim.g.pack_path):joinpath('nvim_config', 'opt', 'note',
+  'autoload'):joinpath('main.py')['filename']
+
+local fts = {
+  'pdf',
+  'html',
+  'docx',
+}
+
+local getfiles = function(directory)
+  local files = {}
+  local result = require("plenary.scandir").scan_dir(directory, { depth = 1, hidden = 1 })
+  for _, file in ipairs(result) do
+    local extension = file:gsub("^.*%.([^.]+)$", "%1")
+    if vim.tbl_contains(fts, extension) == true then
+      table.insert(files, file)
+    end
+  end
+  return files
+end
+
+M.create = function()
+  require('terminal').send('cmd', 'python ' .. vim.g.mkdexportpy .. ' ' .. vim.api.nvim_buf_get_name(0), 0)
+end
+
+M.delete = function()
+  local fpath = require('plenary.path'):new(vim.api.nvim_buf_get_name(0))
+  if fpath:exists() then
+    local curdir = fpath:parent().filename
+    curdir = string.gsub(curdir, '/', '\\')
+    local files = getfiles(curdir)
+    local cnt = 0
+    for _, v in ipairs(files) do
+      cnt = cnt + 1
+      vim.fn.system('del "' .. v .. '"')
+    end
+    print('delete', cnt, 'file(s)')
+  end
+end
+
+return M
--- start/lazy/lua/myplugins/note.lua
+++ start/lazy/lua/myplugins/note.lua
@@ -14,6 +14,9 @@ return {
       { '<F3>p', function() require('mkdimage').getimage('sel_png') end,  mode = { 'n', 'v' }, silent = true, desc = 'getimage sel_png' },
       { '<F3>j', function() require('mkdimage').getimage('sel_jpg') end,  mode = { 'n', 'v' }, silent = true, desc = 'getimage sel_jpg' },
       { '<F3>u', function() require('mkdimage').updatesrc()         end,  mode = { 'n', 'v' }, silent = true, desc = 'updatesrc cur' },
+
+      { '<F3>c', function() require('mkdexport').create()           end,  mode = { 'n', 'v' }, silent = true, desc = 'mkd creqte export' },
+      { '<F3>d', function() require('mkdexport').delete()           end,  mode = { 'n', 'v' }, silent = true, desc = 'mkd delete export' },
     },
   },
 }
