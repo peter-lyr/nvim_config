08c66f4352e0fb2e676580d36e280daec6ba47f7
peter-lyr <llydrp.ldp@gmail.com>
2023-07-09 03:35:00 +0800

    M.xxd_opt default

--- opt/drag/plugin/xxd.lua
+++ opt/drag/plugin/xxd.lua
@@ -2,8 +2,8 @@ local M = {}
 
 M.temp = require('plenary.path'):new(vim.fn.expand('$TEMP')):joinpath('temp_xxd.txt')
 
--- M.xdd_opt = 'xdd -c16 -g2' -- default
-M.xdd_opt = 'xxd -c32 -g2'
+-- M.xxd_opt = 'xxd -c16 -g2' -- default
+M.xxd_opt = 'xxd'
 
 M.dict = {}
 
@@ -22,8 +22,8 @@ vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
       local char = rep(string.format('%s.c', ev.file))
       local ori = ev.file
       local bak = string.format('%s.%s.bak.%s', ev.file, vim.fn.strftime('%Y%m%d%H%M%S'), ext)
-      vim.fn.system(string.format('%s "%s" "%s"', M.xdd_opt, ori, txt))
-      vim.fn.system(string.format('cd %s && %s -i "%s" "%s"', vim.fn.fnamemodify(ori, ':h'), M.xdd_opt,
+      vim.fn.system(string.format('%s "%s" "%s"', M.xxd_opt, ori, txt))
+      vim.fn.system(string.format('cd %s && %s -i "%s" "%s"', vim.fn.fnamemodify(ori, ':h'), M.xxd_opt,
         vim.fn.fnamemodify(ori, ':t'), char))
       vim.fn.system(string.format('copy /y "%s" "%s"', ev.file, bak))
       vim.cmd('e ' .. txt)
@@ -46,9 +46,9 @@ require('maps').add('<F5>', 'n', function()
           vim.cmd('setlocal ft=xxd')
         end
         local txt = vim.api.nvim_buf_get_name(0)
-        vim.fn.system(string.format('%s -r "%s" > "%s" && %s "%s" "%s"', M.xdd_opt, txt, M.dict[txt][1], M.xdd_opt,
+        vim.fn.system(string.format('%s -r "%s" > "%s" && %s "%s" "%s"', M.xxd_opt, txt, M.dict[txt][1], M.xxd_opt,
           M.dict[txt][1], M.temp))
-        vim.fn.system(string.format('cd %s && %s -i "%s" "%s"', vim.fn.fnamemodify(M.dict[txt][1], ':h'), M.xdd_opt,
+        vim.fn.system(string.format('cd %s && %s -i "%s" "%s"', vim.fn.fnamemodify(M.dict[txt][1], ':h'), M.xxd_opt,
           vim.fn.fnamemodify(M.dict[txt][1], ':t'), M.dict[txt][1]))
         local lines = require('plenary.path'):new(M.temp):readlines()
         local len = #lines
