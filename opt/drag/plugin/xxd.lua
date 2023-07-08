bcf7499dc154b90563ce72b74b0dfdce4b6376fa
peter-lyr <llydrp.ldp@gmail.com>
2023-07-09 02:44:42 +0800

    multiple xxd

--- opt/drag/plugin/xxd.lua
+++ opt/drag/plugin/xxd.lua
@@ -5,23 +5,31 @@ M.temp = require('plenary.path'):new(vim.fn.expand('$TEMP')):joinpath('temp_xxd.
 -- M.xdd_opt = 'xdd -c16 -g2' -- default
 M.xdd_opt = 'xxd -c32 -g2'
 
-M.txt = ''
-M.ori = ''
-M.bak = ''
+M.dict = {}
+
+local function rep(content)
+  content = vim.fn.tolower(content)
+  content = string.gsub(content, '\\', '/')
+  return content
+end
 
 vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
   pattern = { "*.jpg" },
   callback = function(ev)
     vim.schedule(function()
       local ext = string.match(ev.file, '.+%.(%w+)$')
-      M.txt = string.format('%s.txt', ev.file)
-      M.ori = ev.file
-      M.bak = string.format('%s.%s.bak.%s', ev.file, vim.fn.strftime('%Y%m%d%H%M%S'), ext)
-      vim.cmd(string.format('!%s "%s" "%s"', M.xdd_opt, ev.file, M.txt))
-      vim.cmd(string.format('!copy /y "%s" "%s"', ev.file, M.bak))
-      vim.cmd('e ' .. M.txt)
+      local txt = rep(string.format('%s.txt', ev.file))
+      local ori = ev.file
+      local bak = string.format('%s.%s.bak.%s', ev.file, vim.fn.strftime('%Y%m%d%H%M%S'), ext)
+      vim.cmd(string.format('!%s "%s" "%s"', M.xdd_opt, ev.file, txt))
+      vim.cmd(string.format('!copy /y "%s" "%s"', ev.file, bak))
+      vim.cmd('e ' .. txt)
       vim.cmd('setlocal ft=xxd')
       vim.cmd(ev.buf .. 'bw!')
+      M.dict[txt] = {
+        ori,
+        bak,
+      }
     end)
   end,
 })
@@ -33,7 +41,8 @@ require('maps').add('<F5>', 'n', function()
         if vim.api.nvim_buf_get_option(vim.fn.bufnr(), 'filetype') ~= 'xxd' then
           vim.cmd('setlocal ft=xxd')
         end
-        vim.fn.system(string.format('%s -r "%s" > "%s" && %s "%s" "%s"', M.xdd_opt, M.txt, M.ori, M.xdd_opt, M.ori, M.temp))
+        local txt = vim.api.nvim_buf_get_name(0)
+        vim.fn.system(string.format('%s -r "%s" > "%s" && %s "%s" "%s"', M.xdd_opt, txt, M.dict[txt][1], M.xdd_opt, M.dict[txt][1], M.temp))
         local lines = require('plenary.path'):new(M.temp):readlines()
         local len = #lines
         vim.fn.setline(1, lines)
