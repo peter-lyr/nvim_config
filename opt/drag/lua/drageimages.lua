d725b98590cde0cdf899e1952bc364315bc50611
peter-lyr <llydrp.ldp@gmail.com>
2023-07-09 04:47:24 +0800

    xxd after mkdimage

--- opt/drag/lua/drageimages.lua
+++ opt/drag/lua/drageimages.lua
@@ -25,15 +25,21 @@ M.readpre = function(ev, last_ev)
   M.last_ev = last_ev
   if M.is_saved_images(ev, last_ev) == true then
     M.flag = 'saved_images'
+    return 1
   else
     M.flag = ''
   end
+  return nil
 end
 
-M.bufenter = function()
+M.bufenter = function(ev)
   if M.flag == 'saved_images' then
     vim.cmd('b' .. M.last_ev.buf)
-    vim.cmd('bw! ' .. M.readpre_ev.buf)
+    vim.loop.new_timer():start(1000, 0, function()
+      vim.schedule(function()
+        vim.cmd('bw! ' .. M.readpre_ev.buf)
+      end)
+    end)
     require('mkdimage').dragimage('sel_png', M.readpre_ev.file)
     vim.cmd('w!')
   end
--- opt/drag/plugin/drag.lua
+++ opt/drag/plugin/drag.lua
@@ -3,6 +3,7 @@ local M = {}
 M.is_dragging = true
 M.is_readpre = false
 M.last_ev = nil
+M.flag = 1
 
 vim.api.nvim_create_autocmd({ 'FocusLost' }, {
   callback = function()
@@ -17,8 +18,20 @@ vim.api.nvim_create_autocmd({ 'BufReadPre' }, {
     vim.schedule(function()
       M.is_readpre = true
       if M.is_dragging == true then
-        require('drageimages').readpre(ev, M.last_ev)
+        M.flag = require('drageimages').readpre(ev, M.last_ev)
       end
+    end)
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
+  callback = function(ev)
+    vim.schedule(function()
+      if M.flag then
+        return
+      end
+      M.flag = 1
+      require('xxd').check(ev)
     end)
   end,
 })
@@ -30,7 +43,7 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
       if M.is_dragging == true and M.is_readpre == true then
         M.is_dragging = false
         M.is_readpre = false
-        require('drageimages').bufenter()
+        require('drageimages').bufenter(ev)
       end
     end)
   end,
