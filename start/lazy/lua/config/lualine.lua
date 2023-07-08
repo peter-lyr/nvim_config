53976f375d82bb96ba20f966a217346342f68193
peter-lyr <llydrp.ldp@gmail.com>
2023-07-08 22:13:40 +0800

    no print

--- start/lazy/lua/config/lualine.lua
+++ start/lazy/lua/config/lualine.lua
@@ -15,8 +15,6 @@ local filename = function(hl_group)
   end
 end
 
-print(require('plenary.path'):new(vim.fn.expand('%:~:.')):absolute())
-
 local function rep(content)
   content = vim.fn.tolower(content)
   content = string.gsub(content, '/', '\\')
