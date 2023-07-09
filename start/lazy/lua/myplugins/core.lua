fc10334ea3035886b2d2b575c67af3ecced95137
peter-lyr <llydrp.ldp@gmail.com>
2023-07-09 17:58:06 +0800

    csc

--- start/lazy/lua/myplugins/core.lua
+++ start/lazy/lua/myplugins/core.lua
@@ -38,6 +38,7 @@ return {
       { 'cs.',                '<cmd>!start %:h<cr>',                                                   mode = { 'n', 'v' },  silent = true, desc = 'start %:h' },
       { 'csu',                '<cmd>!start ..<cr>',                                                    mode = { 'n', 'v' },  silent = true, desc = 'start ..' },
       { 'cs=',                '<cmd>exe "!start " .. ProjectRootGet()<cr>',                            mode = { 'n', 'v' },  silent = true, desc = 'start ProjectRootGet' },
+      { 'csc',                '<cmd>!chcp 65001 && %:p<cr>',                                           mode = { 'n', 'v' },  silent = true, desc = 'system open %:h' },
 
       -- copy_paste
 
