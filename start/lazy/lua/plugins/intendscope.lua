0bbb15fe77d4a783eb82c4595b0a901647251d97
peter-lyr <llydrp.ldp@gmail.com>
2023-07-10 12:47:48 +0800

    check 1M vim.b.miniindentscope_disable

--- start/lazy/lua/plugins/intendscope.lua
+++ start/lazy/lua/plugins/intendscope.lua
@@ -20,6 +20,15 @@ return {
         vim.b.miniindentscope_disable = true
       end,
     })
+    vim.api.nvim_create_autocmd("BufReadPre", {
+      callback = function(ev)
+        local max_filesize = 1000 * 1024
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(ev.buf))
+        if ok and stats and stats.size > max_filesize then
+          vim.b.miniindentscope_disable = true
+        end
+      end,
+    })
   end,
   config = function()
     require('mini.indentscope').setup({
