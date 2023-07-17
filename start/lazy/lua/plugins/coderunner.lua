3718cef7af85f511fb5ea343ad695f58da0c6c08
peter-lyr <llydrp.ldp@gmail.com>
2023-07-17 22:36:29 +0800

    .

--- start/lazy/lua/plugins/coderunner.lua
+++ start/lazy/lua/plugins/coderunner.lua
@@ -12,12 +12,12 @@ return {
       filetype = {
         python = 'python -u',
         c = 'cd $dir && ' ..
-          'taskkill /f /im $fileNameWithoutExt.exe & ' ..
-          'gcc $fileName -Wall -s -ffunction-sections -fdata-sections -Wl,--gc-sections -O3 -o $fileNameWithoutExt && ' ..
-          'strip -s $dir\\$fileNameWithoutExt.exe && ' ..
-          'upx --best $dir\\$fileNameWithoutExt.exe && ' ..
-          'echo ============================================================ && ' ..
-          '$dir\\$fileNameWithoutExt'
+            'taskkill /f /im $fileNameWithoutExt.exe & ' ..
+            'gcc $fileName -Wall -s -ffunction-sections -fdata-sections -Wl,--gc-sections -O3 -o $fileNameWithoutExt && ' ..
+            'strip -s $dir\\$fileNameWithoutExt.exe && ' ..
+            'upx --best $dir\\$fileNameWithoutExt.exe && ' ..
+            'echo ============================================================ && ' ..
+            '$dir\\$fileNameWithoutExt'
       },
     })
   end,
