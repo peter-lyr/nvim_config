92a7a99d39efc84505fe682108dcd0643a5cba63
peter-lyr <llydrp.ldp@gmail.com>
2023-07-19 20:44:02 +0800

    delete shade

--- start/lazy/lua/plugins/shade.lua
+++ start/lazy/lua/plugins/shade.lua
@@ -1,22 +1,3 @@
--- return {
---   'jghauser/shade.nvim',
---   lazy = true,
---   event = { "CursorHold", "CursorHoldI", },
---   config = function()
---     require('shade').setup({
---       overlay_opacity = 50,
---       opacity_step = 1,
---       -- debug = true,
---       keys = {
---         brightness_up   = '<C-Up>',
---         brightness_down = '<C-Down>',
---         toggle          = '<leader>ts',
---       }
---     })
---   end
--- }
-
-
 return {
   "rosstang/dimit.nvim",
   lazy = true,
@@ -25,7 +6,6 @@ return {
     require("dimit").setup()
   end,
 }
-
 
 -- return {
 --   "levouh/tint.nvim",
