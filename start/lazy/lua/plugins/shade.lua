c7b7f3f7cf773b9e46ee42a363da2553a9f8d73e
peter-lyr <llydrp.ldp@gmail.com>
2023-07-19 20:42:55 +0800

    shade dimit or tint ok

--- start/lazy/lua/plugins/shade.lua
+++ start/lazy/lua/plugins/shade.lua
@@ -1,16 +1,37 @@
+-- return {
+--   'jghauser/shade.nvim',
+--   lazy = true,
+--   event = { "CursorHold", "CursorHoldI", },
+--   config = function()
+--     require('shade').setup({
+--       overlay_opacity = 50,
+--       opacity_step = 1,
+--       -- debug = true,
+--       keys = {
+--         brightness_up   = '<C-Up>',
+--         brightness_down = '<C-Down>',
+--         toggle          = '<leader>ts',
+--       }
+--     })
+--   end
+-- }
+
+
 return {
-  -- 'sunjon/shade.nvim',
-  -- lazy = true,
-  -- event = { "CursorHold", "CursorHoldI", },
-  -- config = function()
-  --   require('shade').setup({
-  --     overlay_opacity = 50,
-  --     opacity_step = 1,
-  --     keys = {
-  --       brightness_up   = '<C-Up>',
-  --       brightness_down = '<C-Down>',
-  --       toggle          = '<leader>ts',
-  --     }
-  --   })
-  -- end
+  "rosstang/dimit.nvim",
+  lazy = true,
+  event = { "CursorHold", "CursorHoldI", },
+  config = function()
+    require("dimit").setup()
+  end,
 }
+
+
+-- return {
+--   "levouh/tint.nvim",
+--   lazy = true,
+--   event = { "CursorHold", "CursorHoldI", },
+--   config = function()
+--     require("tint").setup()
+--   end,
+-- }
