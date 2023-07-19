4ba7106ec7419f89542b625f316aed94bfb4e278
peter-lyr <llydrp.ldp@gmail.com>
2023-07-19 13:49:58 +0800

    shade.nvim

---
+++ start/lazy/lua/plugins/shade.lua
@@ -0,0 +1,16 @@
+return {
+  'sunjon/shade.nvim',
+  lazy = true,
+  event = { "CursorHold", "CursorHoldI", },
+  config = function()
+    require('shade').setup({
+      overlay_opacity = 50,
+      opacity_step = 1,
+      keys = {
+        brightness_up   = '<C-Up>',
+        brightness_down = '<C-Down>',
+        toggle          = '<leader>ts',
+      }
+    })
+  end
+}
