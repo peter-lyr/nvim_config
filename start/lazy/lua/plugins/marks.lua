b4dced3d2dfd33f95654737b46122c2f8a8f9a3d
peter-lyr <llydrp.ldp@gmail.com>
2023-07-19 10:56:07 +0800

    marks.nvim excluded_filetypes NvimTree

--- start/lazy/lua/plugins/marks.lua
+++ start/lazy/lua/plugins/marks.lua
@@ -10,7 +10,9 @@ return {
       force_write_shada = true,
       refresh_interval = 250,
       sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
-      excluded_filetypes = {},
+      excluded_filetypes = {
+        'NvimTree',
+      },
       mappings = {
         toggle = "mm",
         set_next = "m,",
