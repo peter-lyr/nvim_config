9de7390b06f77f93ebb68fb2b66bcc208df15ae3
peter-lyr <llydrp.ldp@gmail.com>
2023-07-20 15:46:40 +0800

    local events_log_en

--- start/lazy/lua/init/log.lua
+++ start/lazy/lua/init/log.lua
@@ -1,4 +1,4 @@
-vim.g.events_log_en = 1
+local events_log_en = 1
 local events_log_dir = vim.fn.stdpath('data') .. '\\events_log\\'
 vim.g.events_log = events_log_dir .. vim.fn.strftime("%Y%m%d-%H%M%S.log")
 if vim.fn.isdirectory(events_log_dir) == 0 then
@@ -6,7 +6,7 @@ if vim.fn.isdirectory(events_log_dir) == 0 then
 end
 
 function EventsLog(ev, en)
-  if vim.g.events_log_en == 1 and en == 1 then
+  if events_log_en == 1 and en == 1 then
     if ev and ev.buf and ev.event and ev.file then
       vim.fn.writefile({
         string.format([[%3.3f %-2d %-10s "%s"]],
@@ -17,7 +17,7 @@ function EventsLog(ev, en)
 end
 
 vim.api.nvim_create_user_command('EventsLogOpen', function()
-  if vim.g.events_log_en == 1 then
+  if events_log_en == 1 then
     vim.cmd('e ' .. vim.g.events_log)
   end
 end, { bang = true, nargs = "*", })
