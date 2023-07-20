38b6b569de7de20f938f5d284a800e2b57f46bc0
peter-lyr <llydrp.ldp@gmail.com>
2023-07-20 15:40:37 +0800

    require('init.log')

--- init.lua
+++ init.lua
@@ -5,30 +5,6 @@ local pack = vimruntime .. '\\pack\\'
 
 vim.g.pack_path = pack
 
-vim.g.events_log_en = 1
-local events_log_dir = vim.fn.stdpath('data') .. '\\events_log\\'
-vim.g.events_log = events_log_dir .. vim.fn.strftime("%Y%m%d-%H%M%S.log")
-if vim.fn.isdirectory(events_log_dir) == 0 then
-  vim.fn.system(string.format('md "%s"', events_log_dir))
-end
-
-function Log(ev, en)
-  if vim.g.events_log_en == 1 and en == 1 then
-    if ev and ev.buf and ev.event and ev.file then
-      vim.fn.writefile({
-        string.format([[%3.3f %-2d %-10s "%s"]],
-          os.clock(), ev.buf, ev.event, ev.file)
-      }, vim.g.events_log, 'a')
-    end
-  end
-end
-
-vim.api.nvim_create_user_command('EventsLogOpen', function()
-  if vim.g.events_log_en == 1 then
-    vim.cmd('e ' .. vim.g.events_log)
-  end
-end, { bang = true, nargs = "*", })
-
 local lazypath = pack .. "lazy\\start\\lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -104,3 +80,5 @@ lazy.setup({
     },
   },
 })
+
+require('init.log')
