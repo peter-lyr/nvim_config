47eb18717c4b8e2f0293ee00f3ff36c05c227c29
peter-lyr <llydrp.ldp@gmail.com>
2023-07-22 14:59:18 +0800

    nvimtree-oftendirs.lua

---
+++ start/lazy/lua/config/nvimtree-oftendirs.lua
@@ -0,0 +1,45 @@
+package.loaded['config.nvimtree-oftendirs'] = nil
+
+local M = {}
+
+local nv = require('plenary.path'):new(vim.fn.expand('$VIMRUNTIME')):parent():parent():parent():parent()
+
+local dirs = {
+  vim.fn.expand([[$HOME]]),
+  vim.fn.expand([[$LOCALAPPDATA]]),
+  vim.fn.expand([[$VIMRUNTIME\pack]]),
+  vim.fn.expand([[$VIMRUNTIME\pack\nvim_config]]),
+  vim.fn.expand([[$VIMRUNTIME\pack\lazy\plugins]]),
+  nv.filename,
+}
+
+local existsdirs = {}
+
+for _, dir in ipairs(dirs) do
+  if vim.fn.isdirectory(dir) then
+    table.insert(existsdirs, dir)
+  end
+end
+
+for i = 1, 26 do
+  local dir = vim.fn.nr2char(64 + i) .. [[:\]]
+  if vim.fn.isdirectory(dir) == 1 then
+    table.insert(existsdirs, dir)
+  end
+end
+
+M.open = function()
+  vim.ui.select(vim.fn.sort(existsdirs), { prompt = 'oftendirs' }, function(choice)
+    if not choice then
+      return
+    end
+    vim.cmd('NvimTreeOpen')
+    vim.loop.new_timer():start(10, 0, function()
+      vim.schedule(function()
+        vim.cmd('cd ' .. choice)
+      end)
+    end)
+  end)
+end
+
+return M
--- start/lazy/lua/plugins/nvimtree.lua
+++ start/lazy/lua/plugins/nvimtree.lua
@@ -10,12 +10,14 @@ return {
   keys = {
     { '<leader><tab>', '<cmd>NvimTreeFindFile<cr>', mode = { 'n', 'v', }, silent = true, desc = 'NvimTreeFindFile' },
     { '<leader>q', '<cmd>Git<cr>', mode = { 'n', 'v' }, desc = 'Git' }, -- fugitive
+    { '<leader>be', function() require('config.nvimtree-oftendirs').open() end, mode = { 'n', 'v', }, silent = true, desc = 'nvimtree oftendirs open' },
   },
   dependencies = {
     require('myplugins.core'),   -- terminal
     require('wait.projectroot'),
     require('plugins.fugitive'),
     require('plugins.edgy'),
+    require('wait.plenary'),
   },
   config = function()
     require('config.nvimtree')
