ef1ae31d281732576bbf6553e51a3dc7fa36813d
peter-lyr <llydrp.ldp@gmail.com>
2023-07-24 22:58:47 +0800

    <leader>rvva

--- start/lazy/lua/myplugins/computer.lua
+++ start/lazy/lua/myplugins/computer.lua
@@ -13,21 +13,21 @@ return {
 
       -- 关闭显示器倒计时
 
-      { '<leader><leader>rA', function() require('Windows').monitor_1min()       end, mode = { 'n', 'v' }, silent = true, desc = 'monitor close timeout 1min' },
-      { '<leader><leader>ra', function() require('Windows').monitor_30min()      end, mode = { 'n', 'v' }, silent = true, desc = 'monitor close timeout 30min' },
+      { '<leader>rva',  function() require('Windows').monitor_30min()      end, mode = { 'n', 'v' }, silent = true, desc = 'monitor close timeout 30min' },
+      { '<leader>rvva', function() require('Windows').monitor_1min()       end, mode = { 'n', 'v' }, silent = true, desc = 'monitor close timeout 1min' },
 
       -- 系统代理
 
-      { '<leader><leader>rb', function() require('Windows').proxy_on()           end, mode = { 'n', 'v' }, silent = true, desc = 'proxy_on' },
-      { '<leader><leader>rB', function() require('Windows').proxy_off()          end, mode = { 'n', 'v' }, silent = true, desc = 'proxy_off' },
+      { '<leader>rvb', function() require('Windows').proxy_on()           end, mode = { 'n', 'v' }, silent = true, desc = 'proxy_on' },
+      { '<leader>rvB', function() require('Windows').proxy_off()          end, mode = { 'n', 'v' }, silent = true, desc = 'proxy_off' },
 
       -- 环境变量
 
-      { '<leader><leader>rc', function() require('Windows').path()               end, mode = { 'n', 'v' }, silent = true, desc = 'open env path' },
+      { '<leader>rvc', function() require('Windows').path()               end, mode = { 'n', 'v' }, silent = true, desc = 'open env path' },
 
       -- 声音属性
 
-      { '<leader><leader>rd', function() require('Windows').sound()              end, mode = { 'n', 'v' }, silent = true, desc = 'sound' },
+      { '<leader>rvd', function() require('Windows').sound()              end, mode = { 'n', 'v' }, silent = true, desc = 'sound' },
 
     },
   },
