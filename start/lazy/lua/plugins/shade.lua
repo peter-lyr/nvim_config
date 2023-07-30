d62490a56d21fe7bce0f11d3597b8c6f730aed17
peter-lyr <llydrp.ldp@gmail.com>
2023-07-30 11:40:18 +0800

    CursorHoldI untint

--- start/lazy/lua/plugins/shade.lua
+++ start/lazy/lua/plugins/shade.lua
@@ -13,5 +13,10 @@ return {
   event = { "CursorHold", "CursorHoldI", },
   config = function()
     require("tint").setup()
+    vim.api.nvim_create_autocmd({ "CursorHold", }, {
+      callback = function()
+        require("tint").untint(vim.fn.win_getid())
+      end,
+    })
   end,
 }
