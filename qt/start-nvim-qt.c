c88d0ca507b87b0062ac7ea509aa36501eac2e2c
peter-lyr <llydrp.ldp@gmail.com>
2023-07-31 11:47:59 +0800

    INCLUDE

--- qt/start-nvim-qt.c
+++ qt/start-nvim-qt.c
@@ -1,81 +1,89 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 char *get_parent_dir_do(const char *path) {
-    char *parent_dir = NULL;
-    char *last_slash = strrchr(path, '\\');
-    if (last_slash != NULL) {
-        parent_dir = (char *)malloc(last_slash - path + 1);
-        memcpy(parent_dir, path, last_slash - path);
-        parent_dir[last_slash - path] = '\0';
-    }
-    return parent_dir;
+  char *parent_dir = NULL;
+  char *last_slash = strrchr(path, '\\');
+  if (last_slash != NULL) {
+    parent_dir = (char *)malloc(last_slash - path + 1);
+    memcpy(parent_dir, path, last_slash - path);
+    parent_dir[last_slash - path] = '\0';
+  }
+  return parent_dir;
 }
 
 char *get_parent_dir(const char *path, int level) {
-    char *newpath = get_parent_dir_do(path);
-    level--;
-    for (int i=0; i<level; i++) {
-        newpath = get_parent_dir_do(newpath);
-    }
-    return newpath;
+  char *newpath = get_parent_dir_do(path);
+  level--;
+  for (int i = 0; i < level; i++) {
+    newpath = get_parent_dir_do(newpath);
+  }
+  return newpath;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-    // printf("argv[0]: |%s|\n", argv[0]);
+  // printf("argv[0]: |%s|\n", argv[0]);
 
-    // char *nvimwin64;
-    //
-    // for (int i=1; i<9; i++) {
-    //     nvimwin64 = get_parent_dir(argv[0], i);
-    //     printf("%d: |%s|\n", i, nvimwin64);
-    // }
+  // char *nvimwin64;
+  //
+  // for (int i=1; i<9; i++) {
+  //     nvimwin64 = get_parent_dir(argv[0], i);
+  //     printf("%d: |%s|\n", i, nvimwin64);
+  // }
 
-    // get config path
+  // get config path
 
-    char *config = get_parent_dir(argv[0], 1);
-    // printf("config: %s\n", config);
+  char *config = get_parent_dir(argv[0], 1);
+  // printf("config: %s\n", config);
 
-    // get nvimwin64 dir
+  // get nv
 
-    char *nvimwin64 = get_parent_dir(argv[0], 6);
-    printf("nvimwin64: %s\n", nvimwin64);
+  char *nv = get_parent_dir(argv[0], 7);
+  // printf("nv: %s\n", nv);
+  // nv: D:\nv
 
-    // get localappdata dir
+  // get nvimwin64 dir
 
-    char *pack = get_parent_dir(argv[0], 2);
-    // printf("pack: %s\n", pack);
+  char *nvimwin64 = get_parent_dir(argv[0], 6);
+  // printf("nvimwin64: %s\n", nvimwin64);
 
-    char localappdata[256];
-    // sprintf(localappdata, "%s\\%s", nvimwin64, "localappdata");
-    sprintf(localappdata, "%s\\%s", pack, "localappdata");
-    // printf("localappdata: %s\n", localappdata);
+  // get localappdata dir
 
+  char *pack = get_parent_dir(argv[0], 2);
+  // printf("pack: %s\n", pack);
 
-    // get temp dir
+  char localappdata[256];
+  // sprintf(localappdata, "%s\\%s", nvimwin64, "localappdata");
+  sprintf(localappdata, "%s\\%s", pack, "localappdata");
+  // printf("localappdata: %s\n", localappdata);
 
-    char temp[256];
-    // sprintf(temp, "%s\\%s", nvimwin64, "temp");
-    sprintf(temp, "%s\\%s\\%s", pack, "localappdata", "temp");
-    // printf("temp: %s\n", temp);
+  // get temp dir
 
-    // get nvim.exe
+  char temp[256];
+  // sprintf(temp, "%s\\%s", nvimwin64, "temp");
+  sprintf(temp, "%s\\%s\\%s", pack, "localappdata", "temp");
+  // printf("temp: %s\n", temp);
 
-    char nvimexe[256];
-    sprintf(nvimexe, "%s\\%s", nvimwin64, "bin\\nvim-qt.exe");
-    // printf("nvimexe: %s\n", nvimexe);
+  // get nvim.exe
 
-    // get cmd
+  char nvimexe[256];
+  sprintf(nvimexe, "%s\\%s", nvimwin64, "bin\\nvim-qt.exe");
+  // printf("nvimexe: %s\n", nvimexe);
 
-    char cmd[576];
-    sprintf(cmd, "set LOCALAPPDATA=%s&& set TEMP=%s&& start /d %s /b %s", localappdata, temp, config, nvimexe);
+  // get cmd
 
-    // run cmd to open nvim.exe
+  char cmd[576];
+  sprintf(
+      cmd,
+      "set LOCALAPPDATA=%s&& set TEMP=%s&& set "
+      "INCLUDE=%s\\mingw64\\x86_64-w64-mingw32\\include&& start /d %s /b %s",
+      localappdata, temp, nv, config, nvimexe);
 
-    system(cmd);
+  // run cmd to open nvim.exe
 
-    return 0;
+  system(cmd);
+
+  return 0;
 }
