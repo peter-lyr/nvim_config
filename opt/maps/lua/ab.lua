80069164a63ce3771b036de36f74ec6cf4057226
peter-lyr <llydrp.ldp@gmail.com>
2023-08-02 23:25:06 +0800

    xdt

--- opt/maps/lua/ab.lua
+++ opt/maps/lua/ab.lua
@@ -1,8 +1,8 @@
 vim.cmd([[
-cab cpx sort " 排序
-cab cqc g/^\(.*\)$\n\1$/d " 去重
-cab cpq sort\|g/^\(.*\)$\n\1$/d " 排序+去重
-cab cd =strftime("%Y%m%d")
-cab ct =strftime("%H%M%S")
-cab cdt =strftime("%Y%m%d-%H%M%S")
+cab xpx sort " 排序
+cab xqc g/^\(.*\)$\n\1$/d " 去重
+cab xpq sort\|g/^\(.*\)$\n\1$/d " 排序+去重
+cab xd =strftime("%Y%m%d")
+cab xt =strftime("%H%M%S")
+cab xdt =strftime("%Y%m%d-%H%M%S")
 ]])
