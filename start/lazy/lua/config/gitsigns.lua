5d21f156061b617a5b8b4716e717e961ad06d2aa
peter-lyr <llydrp.ldp@gmail.com>
2023-08-02 14:12:35 +0800

    word_diff = 1

--- start/lazy/lua/config/gitsigns.lua
+++ start/lazy/lua/config/gitsigns.lua
@@ -2,14 +2,12 @@ local rf = function()
   require('config.fugitive').open(1)
 end
 
-local gs = package.loaded.gitsigns
-
 require('gitsigns').setup {
   numhl     = true,
   linehl    = false,
   word_diff = true,
 
-  signs = {
+  signs     = {
     add = { text = "▎" },
     change = { text = "▎" },
     delete = { text = "" },
@@ -19,6 +17,8 @@ require('gitsigns').setup {
   },
 
   on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
     local function map(mode, l, r, opts)
       opts = opts or {}
       opts.buffer = bufnr
@@ -39,14 +39,27 @@ require('gitsigns').setup {
     end, { expr = true, desc = 'Gitsigns prev_hunk' })
 
     -- Actions
-    map('n', '<leader>gs', function() gs.stage_hunk(); rf() end, { desc = 'Gitsigns stage_hunk' })
-    map('v', '<leader>gs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, { desc = 'Gitsigns stage_hunk' })
-    map('n', '<leader>gS', function() gs.stage_buffer(); rf() end, { desc = 'Gitsigns stage_buffer' })
+    map('n', '<leader>gs', function()
+      gs.stage_hunk(); rf()
+    end, { desc = 'Gitsigns stage_hunk' })
+    map('v', '<leader>gs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+      { desc = 'Gitsigns stage_hunk' })
+    map('n', '<leader>gS', function()
+      gs.stage_buffer(); rf()
+    end, { desc = 'Gitsigns stage_buffer' })
 
-    map('n', '<leader>gu', function() gs.undo_stage_hunk(); rf() end, { desc = 'Gitsigns undo_stage_hunk' })
-    map('v', '<leader>gr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') }; rf() end, { desc = 'Gitsigns reset_hunk' })
-    map('n', '<leader>gr', function() gs.reset_hunk(); rf() end, { desc = 'Gitsigns reset_hunk' })
-    map('n', '<leader>gR', function() gs.reset_buffer(); rf() end, { desc = 'Gitsigns reset_buffer' })
+    map('n', '<leader>gu', function()
+      gs.undo_stage_hunk(); rf()
+    end, { desc = 'Gitsigns undo_stage_hunk' })
+    map('v', '<leader>gr', function()
+      gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') }; rf()
+    end, { desc = 'Gitsigns reset_hunk' })
+    map('n', '<leader>gr', function()
+      gs.reset_hunk(); rf()
+    end, { desc = 'Gitsigns reset_hunk' })
+    map('n', '<leader>gR', function()
+      gs.reset_buffer(); rf()
+    end, { desc = 'Gitsigns reset_buffer' })
 
     map('n', '<leader>gp', gs.preview_hunk, { desc = 'Gitsigns preview_hunk' })
     map('n', '<leader>gb', function() gs.blame_line { full = true } end, { desc = 'Gitsigns blame_line' })
@@ -66,19 +79,24 @@ require('gitsigns').setup {
   end
 }
 
+local word_diff = 1
+
 pcall(vim.api.nvim_del_autocmd, vim.g.gitsigns_insertenter)
 
-vim.g.gitsigns_insertenter = vim.api.nvim_create_autocmd({ "InsertEnter", }, {
+vim.g.gitsigns_insertenter = vim.api.nvim_create_autocmd({ "InsertEnter", "CursorMoved", }, {
   callback = function()
-    gs.toggle_word_diff(nil)
+    if word_diff then
+      local gs = package.loaded.gitsigns
+      word_diff = gs.toggle_word_diff(nil)
+    end
   end,
 })
 
 pcall(vim.api.nvim_del_autocmd, vim.g.gitsigns_insertleave)
 
-vim.g.gitsigns_insertleave = vim.api.nvim_create_autocmd({ "InsertLeave", }, {
+vim.g.gitsigns_insertleave = vim.api.nvim_create_autocmd({ "CursorHold", }, {
   callback = function()
-    gs.toggle_word_diff(1)
+    local gs = package.loaded.gitsigns
+    word_diff = gs.toggle_word_diff(1)
   end,
 })
-
