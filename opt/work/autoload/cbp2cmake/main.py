c9326a51bef23a8126a7fcc4551a39d421cfab17
peter-lyr <llydrp.ldp@gmail.com>
2023-08-09 11:05:14 +0800

    format

--- opt/work/autoload/cbp2cmake/main.py
+++ opt/work/autoload/cbp2cmake/main.py
@@ -2,18 +2,26 @@ import os
 import re
 import sys
 
+
 def rep(text):
-    return text.replace('\\', '/').lower()
+    return text.replace("\\", "/").lower()
+
 
 def get_libs_a_dirs(project_root):
     lib_a_files = []
     for root, _, files in os.walk(project_root):
         for file in files:
             if file.endswith(".a"):
-                dir = os.path.dirname(rep(os.path.join(root, file)).replace("\\", "/").replace(project_root, "").strip("/"))
+                dir = os.path.dirname(
+                    rep(os.path.join(root, file))
+                    .replace("\\", "/")
+                    .replace(project_root, "")
+                    .strip("/")
+                )
                 if dir not in lib_a_files:
                     lib_a_files.append(dir)
     return lib_a_files
+
 
 def get_executable_cbp(project_root):
     cbp_files = []
@@ -21,18 +29,21 @@ def get_executable_cbp(project_root):
         for file in files:
             if file.endswith(".cbp"):
                 cbp_files.append(rep(os.path.join(root, file)))
-    executable_cbp = ''
+    executable_cbp = ""
     if len(cbp_files) == 1:
         executable_cbp = cbp_files[0]
     elif len(cbp_files) > 1:
         for cbp_file in cbp_files:
-            if os.path.basename(cbp_file) in ['app.cbp']:
+            if os.path.basename(cbp_file) in ["app.cbp"]:
                 executable_cbp = cbp_file
                 break
         else:
-            res = input('Type number to choose one of cbp as executable\n' + 
-                  '\n'.join([str(i+1) + '. ' + v for i, v in enumerate(cbp_files)]) + '\n'
-                  '>> ')
+            res = input(
+                "Type number to choose one of cbp as executable\n"
+                + "\n".join([str(i + 1) + ". " + v for i, v in enumerate(cbp_files)])
+                + "\n"
+                ">> "
+            )
             try:
                 num = int(res)
                 if num in [i + 1 for i, _ in enumerate(cbp_files)]:
@@ -40,6 +51,7 @@ def get_executable_cbp(project_root):
             except Exception as e:
                 print(e)
     return executable_cbp, cbp_files
+
 
 def get_files_and_dirs(project_root, executable_cbp, executable_cbp_dir):
     patt1 = re.compile('directory="(.+)"')
@@ -49,11 +61,21 @@ def get_files_and_dirs(project_root, executable_cbp, executable_cbp_dir):
     directories = []
     files = []
     for directory in re.findall(patt1, content):
-        directory = os.path.normpath(os.path.join(executable_cbp_dir, directory)).replace("\\", "/").replace(project_root, "").strip("/")
+        directory = (
+            os.path.normpath(os.path.join(executable_cbp_dir, directory))
+            .replace("\\", "/")
+            .replace(project_root, "")
+            .strip("/")
+        )
         if directory not in directories:
             directories.append(directory)
     for file in re.findall(patt2, content):
-        file = os.path.normpath(os.path.join(executable_cbp_dir, file)).replace("\\", "/").replace(project_root, "").strip("/")
+        file = (
+            os.path.normpath(os.path.join(executable_cbp_dir, file))
+            .replace("\\", "/")
+            .replace(project_root, "")
+            .strip("/")
+        )
         if file not in files:
             files.append(file)
     return files, directories
@@ -69,25 +91,30 @@ if __name__ == "__main__":
     executable_cbp_dir = os.path.dirname(executable_cbp)
 
     if executable_cbp:
-        executable_files, executable_dirs = get_files_and_dirs(project_root, executable_cbp, executable_cbp_dir)
-        print(len(executable_files), 'files,', len(executable_dirs), 'dirs')
-        print('executable_cbp:', executable_cbp)
+        executable_files, executable_dirs = get_files_and_dirs(
+            project_root, executable_cbp, executable_cbp_dir
+        )
+        print(len(executable_files), "files,", len(executable_dirs), "dirs")
+        print("executable_cbp:", executable_cbp)
         with open(os.path.join(project_root, "CMakeLists.txt"), "wb") as ff:
             ff.write(b"cmake_minimum_required(VERSION 3.5)\n")
             ff.write(b"set(PROJECT_NAME proj_name)\n")
             ff.write(b"project(${PROJECT_NAME})\n\n")
 
             # executable_cbp
-            ff.write(("add_executable(${PROJECT_NAME}\n               ${PROJECT_SOURCE_DIR}/%s\n              )\n"
-                    % ("\n               ${PROJECT_SOURCE_DIR}/"
-                    .join(executable_files))).encode("utf-8"))
+            ff.write(
+                (
+                    "add_executable(${PROJECT_NAME}\n               ${PROJECT_SOURCE_DIR}/%s\n              )\n"
+                    % ("\n               ${PROJECT_SOURCE_DIR}/".join(executable_files))
+                ).encode("utf-8")
+            )
             dirs = [
                 "target_include_directories(${PROJECT_NAME} PUBLIC ${PROJECT_SOURCE_DIR}/%s)"
-                    % dir.replace("\\", "/")
-                    .replace(project_root, "")
-                    .strip("\\")
-                    .strip("/")
-                    .replace("\\", "/")
+                % dir.replace("\\", "/")
+                .replace(project_root, "")
+                .strip("\\")
+                .strip("/")
+                .replace("\\", "/")
                 for dir in executable_dirs
             ]
             ff.write(("\n".join(dirs).encode("utf-8")) + b"\n\n")
@@ -98,17 +125,25 @@ if __name__ == "__main__":
                     continue
                 other_cbp_dir = os.path.dirname(other_cbp_file)
 
-                other_files, other_dirs = get_files_and_dirs(project_root, other_cbp_file, other_cbp_dir)
-                ff.write(("add_library(%s STATIC ${PROJECT_SOURCE_DIR}/%s)\n"
-                         % (os.path.basename(other_cbp_dir), " ${PROJECT_SOURCE_DIR}/"
-                         .join(other_files))).encode("utf-8"))
+                other_files, other_dirs = get_files_and_dirs(
+                    project_root, other_cbp_file, other_cbp_dir
+                )
+                ff.write(
+                    (
+                        "add_library(%s STATIC ${PROJECT_SOURCE_DIR}/%s)\n"
+                        % (
+                            os.path.basename(other_cbp_dir),
+                            " ${PROJECT_SOURCE_DIR}/".join(other_files),
+                        )
+                    ).encode("utf-8")
+                )
                 dirs = [
                     "target_include_directories(${PROJECT_NAME} PUBLIC ${PROJECT_SOURCE_DIR}/%s)"
-                        % dir.replace("\\", "/")
-                        .replace(project_root, "")
-                        .strip("\\")
-                        .strip("/")
-                        .replace("\\", "/")
+                    % dir.replace("\\", "/")
+                    .replace(project_root, "")
+                    .strip("\\")
+                    .strip("/")
+                    .replace("\\", "/")
                     for dir in other_dirs
                 ]
                 ff.write(("\n".join(dirs).encode("utf-8")) + b"\n\n")
@@ -126,4 +161,5 @@ if __name__ == "__main__":
             "cd "
             + project_root
             # + r' && del /s /q .cache & rd /s /q .cache & del /s /q build & rd /s /q build & cmake -B build -G "MinGW Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && cd build & copy compile_commands.json ..\ /y & cd ..'
-            + r' && del /s /q build\\CMakeCache.txt & cmake -B build -G "MinGW Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && cd build & copy compile_commands.json ..\ /y & cd ..')
+            + r' && del /s /q build\\CMakeCache.txt & cmake -B build -G "MinGW Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && cd build & copy compile_commands.json ..\ /y & cd ..'
+        )
