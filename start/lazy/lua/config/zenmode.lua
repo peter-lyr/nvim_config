e4c6ca1048eb0186d937caaacfea77299597791e
peter-lyr <llydrp.ldp@gmail.com>
2023-08-11 09:51:54 +0800

    zenmode

--- start/lazy/lua/config/zenmode.lua
+++ start/lazy/lua/config/zenmode.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.toggle = function()
-  if vim.fn.filereadable(vim.api.nvim_buf_get_name(0)) == 1 then
+  if vim.fn.filereadable(vim.api.nvim_buf_get_name(0)) == 1 or #vim.api.nvim_buf_get_name(0) == 0 then
     require("zen-mode").toggle({ window = { width = 1 } })
   end
 end
