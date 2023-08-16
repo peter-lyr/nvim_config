9b11bc3f0e468831d32a7eaf4582e99dcbcc8814
peter-lyr <llydrp.ldp@gmail.com>
2023-08-16 22:19:13 +0800

    disable bqf.nvim for au error

--- start/lazy/lua/plugins/bqf.lua
+++ start/lazy/lua/plugins/bqf.lua
@@ -1,16 +1,16 @@
 return {
-  'kevinhwang91/nvim-bqf',
-  lazy = true,
-  event = { 'LspAttach', },
-  keys = {
-    { 'dm', function() require('config.bqf').toggle() end, mode = { 'n', 'v' }, silent = true, desc = 'bqf toggle' },
-  },
-  dependencies = {
-    require('plugins.edgy'),
-    require('wait.blankline'),
-    'benseefeldt/qf-format.nvim',
-  },
-  config = function()
-    require('config.bqf')
-  end,
+  -- 'kevinhwang91/nvim-bqf',
+  -- lazy = true,
+  -- event = { 'LspAttach', },
+  -- keys = {
+  --   { 'dm', function() require('config.bqf').toggle() end, mode = { 'n', 'v' }, silent = true, desc = 'bqf toggle' },
+  -- },
+  -- dependencies = {
+  --   require('plugins.edgy'),
+  --   require('wait.blankline'),
+  --   'benseefeldt/qf-format.nvim',
+  -- },
+  -- config = function()
+  --   require('config.bqf')
+  -- end,
 }
