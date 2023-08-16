59c986d1ac8788d80504aecb6fe60adc14f03c53
peter-lyr <llydrp.ldp@gmail.com>
2023-08-17 01:55:47 +0800

    quickfix ok

---
+++ opt/quickfix/lua/quickfix.lua
@@ -0,0 +1,53 @@
+package.loaded['quickfix'] = nil
+
+local M = {}
+
+vim.g.qf_before_winid = -1
+
+M.toggle = function()
+  if vim.api.nvim_buf_get_option(vim.fn.bufnr(), 'buftype') == 'quickfix' then
+    vim.cmd('ccl')
+    if vim.api.nvim_win_is_valid(vim.g.qf_before_winid) == true then
+      vim.fn.win_gotoid(vim.g.qf_before_winid)
+    end
+  else
+    vim.g.qf_before_winid = vim.fn.win_getid()
+    vim.cmd('copen')
+    vim.cmd('wincmd J')
+    vim.fn.timer_start(100, function()
+      vim.api.nvim_win_set_height(0, 15)
+      vim.cmd('set winfixheight')
+      vim.keymap.set("n", "q", function()
+        vim.schedule(function()
+          vim.cmd('ccl')
+          if vim.api.nvim_win_is_valid(vim.g.qf_before_winid) == true then
+            vim.fn.win_gotoid(vim.g.qf_before_winid)
+          end
+        end)
+      end, { buffer = vim.fn.bufnr(), nowait = true, silent = true })
+    end)
+  end
+end
+
+pcall(vim.api.nvim_del_autocmd, vim.g.events_au_bufenter2)
+
+vim.g.events_au_bufenter2 = vim.api.nvim_create_autocmd({ "BufEnter", }, {
+  callback = function(ev)
+    if vim.api.nvim_buf_get_option(ev.buf, 'buftype') == 'quickfix' then
+      local open = function()
+        local cfile = vim.fn.expand('<cfile>')
+        if vim.fn.filereadable(cfile) == 1 then
+          if vim.api.nvim_win_is_valid(vim.g.qf_before_winid) == true then
+            vim.fn.win_gotoid(vim.g.qf_before_winid)
+            vim.cmd('e ' .. cfile)
+          end
+        else
+        end
+      end
+      vim.keymap.set("n", "o", open, { buffer = ev.buf, nowait = true, silent = true })
+      vim.keymap.set("n", "a", open, { buffer = ev.buf, nowait = true, silent = true })
+    end
+  end,
+})
+
+return M
---
+++ start/lazy/lua/myplugins/quickfix.lua
@@ -0,0 +1,29 @@
+local opt = vim.fn.expand('$VIMRUNTIME') .. '\\pack\\nvim_config\\opt\\'
+
+return {
+  {
+    name = 'quickfix',
+    dir = opt .. 'quickfix',
+    dependencies = {
+      'benseefeldt/qf-format.nvim',
+    },
+    keys = {
+
+      -- toggle
+
+      {
+        'dm',
+        function()
+          require('quickfix').toggle()
+        end,
+        mode = { 'n', 'v', },
+        silent = true,
+        desc = 'qf toggle'
+      },
+
+    },
+    init = function()
+      require('quickfix')
+    end
+  },
+}
