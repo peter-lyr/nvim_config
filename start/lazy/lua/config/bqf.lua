ee0da23372fd549962a4f042e89595e72eaefba8
peter-lyr <llydrp.ldp@gmail.com>
2023-09-03 18:01:08 +0800

    bqf

--- start/lazy/lua/config/bqf.lua
+++ start/lazy/lua/config/bqf.lua
@@ -40,34 +40,4 @@ bqf.setup {
   },
 }
 
-local is_copened = function()
-  for i = 1, vim.fn.winnr '$' do
-    local bnum = vim.fn.winbufnr(i)
-    if vim.fn.getbufvar(bnum, '&buftype') == 'quickfix' then
-      return 1
-    end
-  end
-  return nil
-end
-
-M.toggle = function()
-  if is_copened() then
-    if vim.bo.ft == 'qf' then
-      vim.cmd 'wincmd p'
-      vim.loop.new_timer():start(10, 0, function()
-        vim.schedule(function()
-          if vim.b[vim.fn.bufnr()].neo_tree_source == 'git_status' then
-            vim.cmd 'wincmd l'
-          elseif vim.b[vim.fn.bufnr()].source_buffer then
-            vim.cmd 'wincmd h'
-          end
-        end)
-      end)
-    end
-    vim.cmd 'ccl'
-  else
-    vim.cmd 'copen'
-  end
-end
-
 return M
--- start/lazy/lua/plugins/bqf.lua
+++ start/lazy/lua/plugins/bqf.lua
@@ -1,16 +1,12 @@
 return {
-  -- 'kevinhwang91/nvim-bqf',
-  -- lazy = true,
-  -- event = { 'LspAttach', },
-  -- keys = {
-  --   { 'dm', function() require('config.bqf').toggle() end, mode = { 'n', 'v' }, silent = true, desc = 'bqf toggle' },
-  -- },
-  -- dependencies = {
-  --   require('plugins.edgy'),
-  --   require('wait.blankline'),
-  --   'benseefeldt/qf-format.nvim',
-  -- },
-  -- config = function()
-  --   require('config.bqf')
-  -- end,
+  'kevinhwang91/nvim-bqf',
+  lazy = true,
+  event = { 'LspAttach', },
+  dependencies = {
+    require('plugins.edgy'),
+    require('wait.blankline'),
+  },
+  config = function()
+    require('config.bqf')
+  end,
 }
