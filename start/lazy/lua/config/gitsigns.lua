fce084c6ebfe5010fc564eb803ea9203cfd37feb
peter-lyr <llydrp.ldp@gmail.com>
2023-09-15 09:51:57 +0800

    feat: add gitsigns

---
+++ start/lazy/lua/config/gitsigns.lua
@@ -0,0 +1,168 @@
+package.loaded['config.gitsigns'] = nil
+
+require 'gitsigns'.setup {
+  numhl     = true,
+  linehl    = false,
+  word_diff = true,
+
+  signs     = {
+    add = { text = '+', },
+    change = { text = '~', },
+    delete = { text = '_', },
+    topdelete = { text = '‾', },
+    changedelete = { text = '', },
+    untracked = { text = '?', },
+  },
+  sign_priority = 100;
+}
+
+local word_diff_en = 1
+local word_diff = 1
+local moving = nil
+
+pcall(vim.api.nvim_del_autocmd, vim.g.gitsigns_au_insertenter)
+
+vim.g.gitsigns_au_insertenter = vim.api.nvim_create_autocmd({ 'InsertEnter', 'CursorMoved', }, {
+  callback = function()
+    moving = 1
+    if word_diff then
+      word_diff = require 'gitsigns'.toggle_word_diff(nil)
+    end
+  end,
+})
+
+require 'which-key'.register { ['<leader>gm'] = { name = 'Gitsigns', }, }
+
+pcall(vim.api.nvim_del_autocmd, vim.g.gitsigns_au_insertleave)
+
+vim.g.gitsigns_au_insertleave = vim.api.nvim_create_autocmd({ 'CursorHold', }, {
+  callback = function()
+    moving = nil
+    vim.fn.timer_start(500, function()
+      vim.schedule(function()
+        if not moving then
+          if word_diff_en == 1 then
+            word_diff = require 'gitsigns'.toggle_word_diff(1)
+          end
+        end
+      end)
+    end)
+  end,
+})
+
+M = {}
+
+vim.keymap.set('n', '<leader>j', function()
+  if vim.wo.diff then
+    return ']c'
+  end
+  vim.schedule(function()
+    require 'gitsigns'.next_hunk()
+  end)
+  return '<Ignore>'
+end, { expr = true, desc = 'Gitsigns next_hunk', })
+
+vim.keymap.set('n', '<leader>k', function()
+  if vim.wo.diff then
+    return '[c'
+  end
+  vim.schedule(function()
+    require 'gitsigns'.prev_hunk()
+  end)
+  return '<Ignore>'
+end, { expr = true, desc = 'Gitsigns prev_hunk', })
+
+M.next_hunk = function()
+  if vim.wo.diff then
+    return ']c'
+  end
+  vim.schedule(function()
+    require 'gitsigns'.next_hunk()
+  end)
+  return '<Ignore>'
+end
+
+M.prev_hunk = function()
+  if vim.wo.diff then
+    return '[c'
+  end
+  vim.schedule(function()
+    require 'gitsigns'.prev_hunk()
+  end)
+  return '<Ignore>'
+end
+
+M.stage_hunk = function()
+  require 'gitsigns'.stage_hunk()
+end
+
+M.stage_hunk_v = function()
+  require 'gitsigns'.stage_hunk { vim.fn.line '.', vim.fn.line 'v', }
+end
+
+M.stage_buffer = function()
+  require 'gitsigns'.stage_buffer()
+end
+
+M.undo_stage_hunk = function()
+  require 'gitsigns'.undo_stage_hunk()
+end
+
+M.reset_hunk = function()
+  require 'gitsigns'.reset_hunk { vim.fn.line '.', vim.fn.line 'v', }
+end
+
+M.reset_hunk_v = function()
+  require 'gitsigns'.reset_hunk()
+end
+
+M.reset_buffer = function()
+  require 'gitsigns'.reset_buffer()
+end
+
+M.preview_hunk = function()
+  require 'gitsigns'.preview_hunk()
+end
+
+M.blame_line = function()
+  require 'gitsigns'.blame_line { full = true, }
+end
+
+M.diffthis = function()
+  require 'gitsigns'.diffthis()
+end
+
+M.diffthis_l = function()
+  require 'gitsigns'.diffthis '~'
+end
+
+M.toggle_current_line_blame = function()
+  require 'gitsigns'.toggle_current_line_blame()
+end
+
+M.toggle_deleted = function()
+  require 'gitsigns'.toggle_deleted()
+end
+
+M.toggle_numhl = function()
+  require 'gitsigns'.toggle_numhl()
+end
+
+M.toggle_linehl = function()
+  require 'gitsigns'.toggle_linehl()
+end
+
+M.toggle_signs = function()
+  require 'gitsigns'.toggle_signs()
+end
+
+M.toggle_word_diff = function()
+  local temp = require 'gitsigns'.toggle_word_diff()
+  if temp == false then
+    word_diff_en = 0
+  else
+    word_diff_en = 1
+  end
+end
+
+return M
---
+++ start/lazy/lua/plugins/gitsigns.lua
@@ -0,0 +1,36 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  event = { 'BufReadPre', 'BufNewFile', },
+  keys = {
+    '<leader>g',
+
+    { 'ig',           ':<C-U>Gitsigns select_hunk<CR>',                                     mode = { 'o', 'x', }, silent = true, desc = 'Gitsigns select_hunk', },
+    { 'ag',           ':<C-U>Gitsigns select_hunk<CR>',                                     mode = { 'o', 'x', }, silent = true, desc = 'Gitsigns select_hunk', },
+    { '<leader>j',    desc = 'Gitsigns next_hunk', },
+    { '<leader>k',    desc = 'Gitsigns prev_hunk', },
+    { '<leader>gs',   function() require 'config.gitsigns'.stage_hunk() end,                mode = { 'n', },      silent = true, desc = 'Gitsigns stage_hunk', },
+    { '<leader>gs',   function() require 'config.gitsigns'.stage_hunk_l() end,              mode = { 'v', },      silent = true, desc = 'Gitsigns stage_hunk', },
+    { '<leader>gms',  function() require 'config.gitsigns'.stage_buffer() end,              mode = { 'n', },      silent = true, desc = 'Gitsigns stage_buffer', },
+    { '<leader>gu',   function() require 'config.gitsigns'.undo_stage_hunk() end,           mode = { 'n', },      silent = true, desc = 'Gitsigns undo_stage_hunk', },
+    { '<leader>gr',   function() require 'config.gitsigns'.reset_hunk() end,                mode = { 'v', },      silent = true, desc = 'Gitsigns reset_hunk', },
+    { '<leader>gr',   function() require 'config.gitsigns'.reset_hunk_v() end,              mode = { 'n', },      silent = true, desc = 'Gitsigns reset_hunk', },
+    { '<leader>gmr',  function() require 'config.gitsigns'.reset_buffer() end,              mode = { 'n', },      silent = true, desc = 'Gitsigns reset_buffer', },
+    { '<leader>gmp',  function() require 'config.gitsigns'.preview_hunk() end,              mode = { 'n', },      silent = true, desc = 'Gitsigns preview_hunk', },
+    { '<leader>gmb',  function() require 'config.gitsigns'.blame_line() end,                mode = { 'n', },      silent = true, desc = 'Gitsigns blame_line', },
+    { '<leader>gd',   function() require 'config.gitsigns'.diffthis() end,                  mode = { 'n', },      silent = true, desc = 'Gitsigns diffthis', },
+    { '<leader>gmd',  function() require 'config.gitsigns'.diffthis_l() end,                mode = { 'n', },      silent = true, desc = 'Gitsigns diffthis', },
+    { '<leader>gmtb', function() require 'config.gitsigns'.toggle_current_line_blame() end, mode = { 'n', },      silent = true, desc = 'Gitsigns toggle_current_line_blame', },
+    { '<leader>gmtd', function() require 'config.gitsigns'.toggle_deleted() end,            mode = { 'n', },      silent = true, desc = 'Gitsigns toggle_deleted', },
+    { '<leader>gmtn', function() require 'config.gitsigns'.toggle_numhl() end,              mode = { 'n', },      silent = true, desc = 'Gitsigns toggle_numhl', },
+    { '<leader>gmtl', function() require 'config.gitsigns'.toggle_linehl() end,             mode = { 'n', },      silent = true, desc = 'Gitsigns toggle_linehl', },
+    { '<leader>gmts', function() require 'config.gitsigns'.toggle_signs() end,              mode = { 'n', },      silent = true, desc = 'Gitsigns toggle_signs', },
+    { '<leader>gmtw', function() require 'config.gitsigns'.toggle_word_diff() end,          mode = { 'n', },      silent = true, desc = 'Gitsigns toggle_word_diff', },
+  },
+  lazy = true,
+  dependencies = {
+    require 'plugins.whichkey',
+  },
+  config = function()
+    require 'config.gitsigns'
+  end,
+}
