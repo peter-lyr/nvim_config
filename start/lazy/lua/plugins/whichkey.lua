e4646a9a44a14023188772c668981ec56b0d5e3a
peter-lyr <llydrp.ldp@gmail.com>
2023-09-15 09:59:39 +0800

    feat: whichkey add leader g prefix

--- start/lazy/lua/plugins/whichkey.lua
+++ start/lazy/lua/plugins/whichkey.lua
@@ -1,10 +1,12 @@
 return {
   'folke/which-key.nvim',
-  event = "VeryLazy",
+  event = 'VeryLazy',
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
-  opts = {
-  },
+  config = function()
+    require 'which-key'.setup {}
+    require 'which-key'.register { ['<leader>g'] = { name = 'Git prefix', }, }
+  end,
 }
