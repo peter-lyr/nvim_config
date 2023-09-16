f0dbd259b2ba4054abd98e7e3d10d4cf3fdc7ead
peter-lyr <llydrp.ldp@gmail.com>
2023-09-17 02:22:44 +0800

    feat:vim.opt.laststatus = 3

--- opt/core/plugin/options.lua
+++ opt/core/plugin/options.lua
@@ -46,3 +46,4 @@ vim.opt.completeopt   = 'menu,menuone,noselect'
 vim.opt.conceallevel  = 3 -- Hide * markup for bold and italic
 vim.opt.list          = true
 vim.opt.shada         = [[!,'1000,<500,s10000,h]]
+vim.opt.laststatus    = 3
