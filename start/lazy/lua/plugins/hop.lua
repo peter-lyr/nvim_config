f060ddaf8549dc6a996d68987d0f274ad5ef5deb
peter-lyr <llydrp.ldp@gmail.com>
2023-09-17 23:48:55 +0800

    feat: add hop.lua

---
+++ start/lazy/lua/plugins/hop.lua
@@ -0,0 +1,12 @@
+return {
+  'phaazon/hop.nvim',
+  lazy = true,
+  keys = {
+    { 's', ':HopChar1<cr>', mode = { 'n', }, silent = true, desc = 'HopChar1', },
+  },
+  config = function()
+    require 'hop'.setup {
+      keys = 'asdghklqwertyuiopzxcvbnmfj',
+    }
+  end,
+}
