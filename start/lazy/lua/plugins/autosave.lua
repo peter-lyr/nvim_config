18bb22eda06ea48db605447b1b1f0a653e14ca6d
peter-lyr <llydrp.ldp@gmail.com>
2023-09-17 23:32:20 +0800

    feat: add autosave.lua

---
+++ start/lazy/lua/plugins/autosave.lua
@@ -0,0 +1,15 @@
+return {
+  'Pocco81/auto-save.nvim',
+  lazy = true,
+  event = { 'InsertEnter', 'TextChanged', },
+  config = function()
+    require 'auto-save'.setup {
+      execution_message = {
+        message = function()
+          -- return ('AutoSave: saved at ' .. vim.fn.strftime '%H:%M:%S')
+          return ''
+        end,
+      },
+    }
+  end,
+}
