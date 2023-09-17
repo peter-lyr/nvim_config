dc78784f05e16fb33acce97ab14f82a7c2e992e1
peter-lyr <llydrp.ldp@gmail.com>
2023-09-18 00:07:52 +0800

    feat: add marks.lua

---
+++ start/lazy/lua/plugins/marks.lua
@@ -0,0 +1,30 @@
+return {
+  'chentoast/marks.nvim',
+  lazy = true,
+  event = { 'BufReadPost', 'BufNewFile', },
+  config = function()
+    require 'marks'.setup {
+      default_mappings = false,
+      builtin_marks = { '.', '<', '>', '^', },
+      cyclic = false,
+      force_write_shada = true,
+      refresh_interval = 250,
+      sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20, },
+      excluded_filetypes = {
+        -- 'NvimTree',
+        'fugitive',
+      },
+      mappings = {
+        toggle = 'mm',
+        set_next = 'm,',
+        delete_line = 'md',
+        delete_buf = 'mx',
+        next = 'ms',
+        prev = 'mw',
+        annotate = 'ma',
+      },
+    }
+    vim.keymap.set({ 'n', 'v', }, 'mq', '<cmd>MarksQFListBuf<cr>', { desc = 'MarksQFListBuf', })
+    vim.keymap.set({ 'n', 'v', }, 'ma', '<cmd>MarksQFListAll<cr>', { desc = 'MarksQFListAll', })
+  end,
+}
