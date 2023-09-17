6644bf64c6690eacfbcb154afded424a6990f5bc
peter-lyr <llydrp.ldp@gmail.com>
2023-09-18 00:14:02 +0800

    feat: add prettyfold.lua

---
+++ start/lazy/lua/plugins/prettyfold.lua
@@ -0,0 +1,9 @@
+return {
+  'anuvyklack/pretty-fold.nvim',
+  lazy = true,
+  keys = { 'zm', 'zM', 'zc', 'zf', },
+  config = function()
+    vim.opt.foldmethod = 'indent'
+    require 'pretty-fold'.setup()
+  end,
+}
