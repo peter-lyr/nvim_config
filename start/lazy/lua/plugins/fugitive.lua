b953715cf96e5643a68751187ede28a329c39d48
peter-lyr <llydrp.ldp@gmail.com>
2023-09-19 00:12:06 +0800

    feat: cmd Git

--- start/lazy/lua/plugins/fugitive.lua
+++ start/lazy/lua/plugins/fugitive.lua
@@ -1,6 +1,7 @@
 return {
   'tpope/vim-fugitive',
   lazy = true,
+  cmd = { 'Git', },
   keys = {
     { '<leader>g<leader>', function() require 'config.fugitive'.toggle() end, mode = { 'n', 'v', }, silent = true, desc = 'Git toggle', },
   },
