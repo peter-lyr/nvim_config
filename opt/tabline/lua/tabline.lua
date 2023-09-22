e127851d1e1497dab1a17a89b1e3b4d3840fb14e
peter-lyr <llydrp.ldp@gmail.com>
2023-09-22 10:24:57 +0800

    fix: refresh_tabline err on diffview on startup

--- opt/tabline/lua/tabline.lua
+++ opt/tabline/lua/tabline.lua
@@ -174,6 +174,9 @@ M.refresh_tabline = function(hl)
   if projects[cur_projectroot] then
     local items = {}
     local buf_to_show = M.get_buf_to_show(projects[cur_projectroot], vim.fn.bufnr())
+    if #buf_to_show == 0 then
+      return
+    end
     local yy = indexof(projects[cur_projectroot], buf_to_show[1])
     for i, bufnr in ipairs(buf_to_show) do
       local xx = yy + i - 1
