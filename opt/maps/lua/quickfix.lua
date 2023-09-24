7b78a5540a103e059dd27cb90d08099b8fc7f1d3
peter-lyr <llydrp.ldp@gmail.com>
2023-09-24 17:14:22 +0800

    feat: quickfix.lua copen simple

--- opt/maps/lua/quickfix.lua
+++ opt/maps/lua/quickfix.lua
@@ -1,17 +1,7 @@
 local M = {}
 
 M.open = function()
-  local fname = vim.api.nvim_buf_get_name(vim.fn.bufnr())
-  if vim.fn.filereadable(fname) == 1 then
-    vim.cmd 'wincmd v'
-    vim.cmd 'wincmd J'
-    vim.cmd 'copen'
-    vim.cmd 'wincmd k'
-    vim.cmd 'close'
-    vim.cmd 'wincmd j'
-  else
-    print('File not readable, can not open quickfix list')
-  end
+  vim.cmd 'copen'
 end
 
 return M
