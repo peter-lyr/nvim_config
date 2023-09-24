0302ad4c6dfd4e6f2c8a37db0ee817ddad095b0f
peter-lyr <llydrp.ldp@gmail.com>
2023-09-24 17:12:27 +0800

    feat: use echasnovski/mini.map instead

--- start/lazy/lua/config/minimap.lua
+++ start/lazy/lua/config/minimap.lua
@@ -1,62 +1,21 @@
-local M = {}
+local minimap = require 'mini.map'
 
-M.MinimapUpdateHighlight_timer = 0
-M.last_rescan_file = ''
-M.opened = nil
-
-pcall(vim.api.nvim_del_autocmd, vim.g.minimap_au_bufenter)
-
-vim.g.minimap_au_bufenter = vim.api.nvim_create_autocmd('BufEnter', {
-  callback = function(ev)
-    if M.last_rescan_file ~= ev.file and vim.fn.filereadable(ev.file) == 1 then
-      if M.MinimapUpdateHighlight_timer ~= 0 then
-        M.MinimapUpdateHighlight_timer:stop()
-      end
-      M.MinimapUpdateHighlight_timer = vim.loop.new_timer()
-      M.MinimapUpdateHighlight_timer:start(300, 0, function()
-        vim.schedule(function()
-          M.last_rescan_file = ev.file
-          pcall(vim.cmd, 'MinimapUpdateHighlight')
-          M.MinimapUpdateHighlight_timer = 0
-        end)
-      end)
-    elseif vim.api.nvim_buf_get_option(ev.buf, 'filetype') == 'minimap' then
-      vim.opt.cursorcolumn = false
-      if vim.g.minimap_width ~= vim.opt.winwidth:get() then
-        vim.cmd '999wincmd <'
-        vim.opt.scrolloff = 999
-        vim.api.nvim_win_set_width(0, vim.g.minimap_width)
-      end
-    end
-  end,
-})
-
-pcall(vim.api.nvim_del_autocmd, vim.g.minimap_au_tableave_1)
-
-vim.g.minimap_au_tableave_1 = vim.api.nvim_create_autocmd({ 'TabLeave', }, {
-  callback = function()
-    pcall(vim.cmd, 'MinimapClose')
-  end,
-})
-
-pcall(vim.api.nvim_del_autocmd, vim.g.minimap_au_tabenter_1)
-
-vim.g.minimap_au_tabenter_1 = vim.api.nvim_create_autocmd({ 'TabEnter', }, {
-  callback = function()
-    if M.opened then
-      vim.cmd 'Minimap'
-    end
-  end,
-})
-
-M.open = function()
-  vim.cmd 'Minimap'
-  M.opened = 1
-end
-
-M.close = function()
-  pcall(vim.cmd, 'MinimapClose')
-  M.opened = nil
-end
-
-return M
+minimap.setup {
+  integrations = {
+    minimap.gen_integration.builtin_search(),
+    minimap.gen_integration.gitsigns(),
+    minimap.gen_integration.diagnostic(),
+  },
+  symbols = {
+    encode = minimap.gen_encode_symbols.dot('4x2'),
+    scroll_line = '█',
+    scroll_view = '┃',
+  },
+  window = {
+    focusable = false,
+    side = 'right',
+    show_integration_count = true,
+    width = 12,
+    winblend = 25,
+  },
+}
--- start/lazy/lua/plugins/minimap.lua
+++ start/lazy/lua/plugins/minimap.lua
@@ -1,23 +1,14 @@
 return {
-  'peter-lyr/minimap.vim',
-  -- 'wfxr/minimap.vim',
-  lazy = true,
+  'echasnovski/mini.map',
+  version = '*',
   event = { 'BufReadPost', 'BufNewFile', },
-  cmd = { 'Minimap', 'MinimapToggle', },
+  lazy = true,
   keys = {
-    { '<leader>am', function() require 'config.minimap'.open() end,  mode = { 'n', 'v', }, silent = true, desc = 'Minimap', },
-    { '<leader>aM', function() require 'config.minimap'.close() end, mode = { 'n', 'v', }, silent = true, desc = 'MinimapClose', },
-  },
-  dependencies = {
-    'wfxr/code-minimap',
-    require 'plugins.whichkey',
+    { '<leader>am', function() MiniMap.toggle() end,       mode = { 'n', 'v', }, silent = true, desc = 'MiniMap toggle', },
+    { '<leader>an', function() MiniMap.toggle_focus() end, mode = { 'n', 'v', }, silent = true, desc = 'MiniMap toggle_focus', },
   },
   init = function()
     require 'which-key'.register { ['<leader>a'] = { name = 'Side Panel', }, }
-    vim.g.minimap_width = 10
-    vim.g.minimap_block_filetypes = { 'NvimTree', 'help', 'fugitive', }
-    vim.g.minimap_block_buftypes = { 'quickfix', 'prompt', }
-    vim.g.minimap_background_processing = 1
   end,
   config = function()
     require 'config.minimap'
