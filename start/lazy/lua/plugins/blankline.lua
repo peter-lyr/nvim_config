c9a59e74876df0734e116cc1184288ce6cb1881a
peter-lyr <llydrp.ldp@gmail.com>
2023-09-25 23:01:43 +0800

    fix: blankline event CursorHold

--- start/lazy/lua/plugins/blankline.lua
+++ start/lazy/lua/plugins/blankline.lua
@@ -1,7 +1,7 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
   lazy = true,
-  event = { 'BufReadPost', 'BufNewFile', },
+  event = { 'CursorHold', 'CursorHoldI', },
   opt = {
     space_char_blankline = ' ',
     char = '│',
