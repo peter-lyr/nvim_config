f837544c660007a5e832167a755797a6430d000b
peter-lyr <llydrp.ldp@gmail.com>
2023-09-26 14:35:08 +0800

    style: marks exclude NvimTree

--- start/lazy/lua/plugins/marks.lua
+++ start/lazy/lua/plugins/marks.lua
@@ -12,7 +12,7 @@ return {
       refresh_interval = 250,
       sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20, },
       excluded_filetypes = {
-        -- 'NvimTree',
+        'NvimTree',
         'fugitive',
       },
       mappings = {
