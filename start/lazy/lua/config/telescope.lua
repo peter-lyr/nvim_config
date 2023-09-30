65d1a8c51457b7a7854e147ceb15cd327a503607
peter-lyr <llydrp.ldp@gmail.com>
2023-09-30 22:50:26 +0800

    style: telescope M.ui_all

--- start/lazy/lua/config/telescope.lua
+++ start/lazy/lua/config/telescope.lua
@@ -629,6 +629,12 @@ for i = 1, #TelescopeKeysUiAll do
 end
 
 M.ui_all = function()
+  local descs = {}
+  local keys = {}
+  for i = 1, #TelescopeKeysUiAll do
+    descs[#descs + 1] = TelescopeKeysUiAll[i]['desc']
+    keys[#keys + 1] = vim.fn.substitute(TelescopeKeysUiAll[i][1], '<leader>', ' ', 'g')
+  end
   vim.ui.select(descs, { prompt = 'telescope all', }, function(choice, idx)
     if not choice then
       return
