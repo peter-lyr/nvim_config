43e633c3671f137be080b4ccb1b5a98d7074a2d1
peter-lyr <llydrp.ldp@gmail.com>
2023-10-02 18:44:09 +0800

    style: map <leader>r<leader> run

--- start/lazy/lua/plugins/coderunner.lua
+++ start/lazy/lua/plugins/coderunner.lua
@@ -3,15 +3,15 @@ return {
   lazy = true,
   commit = '3c365982',
   keys = {
-    { '<leader>rr',     function() require 'config.coderunner'.run() end,         mode = { 'n', 'v', }, silent = true, desc = 'code runner run', },
-    { '<leader>rb',     function() require 'config.coderunner'.build() end,       mode = { 'n', 'v', }, silent = true, desc = 'code runner build', },
-    { '<leader>rB',     function() require 'config.coderunner'.rebuild() end,     mode = { 'n', 'v', }, silent = true, desc = 'code runner rebuild', },
-    { '<leader>r<c-b>', function() require 'config.coderunner'.build_run() end,   mode = { 'n', 'v', }, silent = true, desc = 'code runner build_run', },
-    { '<leader>rR',     function() require 'config.coderunner'.rebuild_run() end, mode = { 'n', 'v', }, silent = true, desc = 'code runner rebuild_run', },
+    { '<leader>rb',        function() require 'config.coderunner'.build() end,       mode = { 'n', 'v', }, silent = true, desc = 'code runner build', },
+    { '<leader>rB',        function() require 'config.coderunner'.rebuild() end,     mode = { 'n', 'v', }, silent = true, desc = 'code runner rebuild', },
+    { '<leader>r<leader>', function() require 'config.coderunner'.run() end,         mode = { 'n', 'v', }, silent = true, desc = 'code runner run', },
+    { '<leader>rr',        function() require 'config.coderunner'.build_run() end,   mode = { 'n', 'v', }, silent = true, desc = 'code runner build_run', },
+    { '<leader>rR',        function() require 'config.coderunner'.rebuild_run() end, mode = { 'n', 'v', }, silent = true, desc = 'code runner rebuild_run', },
 
     --c
-    { '<c-f9>',         function() require 'config.coderunner_c'.build() end,     mode = { 'n', 'v', }, silent = true, desc = 'code runner c to_make and build', },
-    { '<c-f10>',        function() require 'config.coderunner_c'.to_cmake() end,  mode = { 'n', 'v', }, silent = true, desc = 'code runner c to_cmake', },
+    { '<c-f9>',            function() require 'config.coderunner_c'.build() end,     mode = { 'n', 'v', }, silent = true, desc = 'code runner c to_make and build', },
+    { '<c-f10>',           function() require 'config.coderunner_c'.to_cmake() end,  mode = { 'n', 'v', }, silent = true, desc = 'code runner c to_cmake', },
   },
   dependencies = {
     require 'plugins.projectroot',
