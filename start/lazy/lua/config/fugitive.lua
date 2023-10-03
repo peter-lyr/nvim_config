406fd873989695b5399d79dc1af3bebb6a2b5194
peter-lyr <llydrp.ldp@gmail.com>
2023-10-03 11:16:21 +0800

    feat: fugitive map dd X

--- start/lazy/lua/config/fugitive.lua
+++ start/lazy/lua/config/fugitive.lua
@@ -12,4 +12,18 @@ M.toggle = function()
   end
 end
 
+local function feed(key)
+  vim.cmd(string.format([[call feedkeys("%s")]], key))
+end
+
+pcall(vim.api.nvim_del_autocmd, vim.g.fugitive_au_bufenter)
+
+vim.g.fugitive_au_bufenter = vim.api.nvim_create_autocmd({ 'BufEnter', }, {
+  callback = function(ev)
+    if vim.bo.ft == 'fugitive' then
+      vim.keymap.set({ 'n', 'v', }, 'dd', function() feed 'X' end, { desc = 'X', buffer = ev.buf, })
+    end
+  end,
+})
+
 return M
