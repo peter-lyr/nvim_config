bf4ccd71c1d07d5361174b2fc02afe2b2c41c52c
peter-lyr <llydrp.ldp@gmail.com>
2023-10-13 16:38:26 +0800

    feat: coderunner.lua run proj

--- start/lazy/lua/config/coderunner.lua
+++ start/lazy/lua/config/coderunner.lua
@@ -2,6 +2,17 @@ package.loaded['config.coderunner'] = nil
 
 local M = {}
 
+M.numberofcores = 1
+
+local f = io.popen 'wmic cpu get NumberOfCores'
+for dir in string.gmatch(f:read '*a', '([%S ]+)') do
+  local NumberOfCores = vim.fn.str2nr(vim.fn.trim(dir))
+  if NumberOfCores > 0 then
+    M.numberofcores = NumberOfCores
+  end
+end
+f:close()
+
 M.merge_tables = function(...)
   local result = {}
   for _, t in ipairs { ..., } do
@@ -15,7 +26,7 @@ end
 local function system_cd(p)
   local s = ''
   if string.sub(p, 2, 2) == ':' then
-    s = s .. string.sub(p, 1, 2)
+    s = s .. string.sub(p, 1, 2) .. ' & '
   end
   if require 'plenary.path'.new(p):is_dir() then
     s = s .. 'cd ' .. p
@@ -53,10 +64,6 @@ M.cmd_run = function(dir, fileNameWithoutExt)
   end
 end
 
-M.cmd_build = {
-  'gcc $fileName -Wall -s -ffunction-sections -fdata-sections -Wl,--gc-sections -O3 -o $fileNameWithoutExt',
-}
-
 M.cmd_compress = function(dir, fileNameWithoutExt)
   if dir and fileNameWithoutExt then
     return {
@@ -71,18 +78,29 @@ M.cmd_compress = function(dir, fileNameWithoutExt)
   end
 end
 
--- local a = {
---   M.rebuild_en and
---   string.format('%s & del /s /q .cache & rd /s /q .cache & del /s /q build & rd /s /q build & cmake -B build -G "MinGW Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 & cd build', systemcd(dir))
---   or string.format('%s & cmake -B build -G "MinGW Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 & cd build', systemcd(dir)),
---   string.format('taskkill /f /im %s.exe & mingw32-make -j%d', curname, M.numberofcores * 2),
---   string.format('strip -s %s.exe & cd .', projname),
---   string.format('upx -qq --best %s.exe & cd .', projname),
---   string.format('copy /y %s.exe ..\\%s.exe', projname, curname),
---   'copy /y compile_commands.json ..\\compile_commands.json',
---   'cd ..',
---   string.format('%s.exe', curname),
--- }
+M.cmd_build = function(dir, projname, curname)
+  if dir and projname and curname then
+    local cmd = {}
+    cmd[#cmd + 1] = system_cd(dir)
+    if M.rebuild_en then
+      cmd[#cmd + 1] = 'del /s /q .cache'
+      cmd[#cmd + 1] = 'rd  /s /q .cache'
+      cmd[#cmd + 1] = 'del /s /q build'
+      cmd[#cmd + 1] = 'rd  /s /q build'
+    end
+    cmd[#cmd + 1] = 'cmake -B build -G "MinGW Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=1'
+    cmd[#cmd + 1] = 'cd build'
+    cmd[#cmd + 1] = string.format('mingw32-make -j%d', M.numberofcores * 2)
+    cmd[#cmd + 1] = string.format('copy /y %s.exe ..\\%s.exe', projname, curname)
+    cmd[#cmd + 1] = 'copy /y compile_commands.json ..\\compile_commands.json'
+    cmd[#cmd + 1] = 'cd ..'
+    return cmd
+  else
+    return {
+      'gcc $fileName -Wall -s -ffunction-sections -fdata-sections -Wl,--gc-sections -O3 -o $fileNameWithoutExt',
+    }
+  end
+end
 
 M.get_cmd = function(...)
   local cmd = vim.fn.join(M.merge_tables(...), ' & echo ========== & ')
@@ -90,15 +108,49 @@ M.get_cmd = function(...)
 end
 
 M.get_cmd_run = function()
-  return M.get_cmd(M.cmd_run())
+  return M.get_cmd(
+    M.cmd_run()
+  )
 end
 
 M.get_cmd_build = function()
-  return M.get_cmd(M.cmd_pre(), M.cmd_compress(), M.cmd_build)
+  return M.get_cmd(
+    M.cmd_pre(),
+    M.cmd_compress(),
+    M.cmd_build()
+  )
 end
 
 M.get_cmd_build_run = function()
-  return M.get_cmd(M.cmd_pre(), M.cmd_compress(), M.cmd_build, M.cmd_run())
+  return M.get_cmd(
+    M.cmd_pre(),
+    M.cmd_compress(),
+    M.cmd_build(),
+    M.cmd_run()
+  )
+end
+
+M.get_cmd_run_proj = function()
+  return M.get_cmd(
+    M.cmd_run(M.dir, M.curname)
+  )
+end
+
+M.get_cmd_build_proj = function()
+  return M.get_cmd(
+    M.cmd_pre(M.dir, M.curname),
+    M.cmd_build(M.dir, M.projname, M.curname),
+    M.cmd_compress(M.dir, M.curname)
+  )
+end
+
+M.get_cmd_build_run_proj = function()
+  return M.get_cmd(
+    M.cmd_pre(M.dir, M.curname),
+    M.cmd_build(M.dir, M.projname, M.curname),
+    M.cmd_compress(M.dir, M.curname),
+    M.cmd_run(M.dir, M.curname)
+  )
 end
 
 M.setup_ft = function(ft)
@@ -107,8 +159,21 @@ M.setup_ft = function(ft)
   }
 end
 
+M.setup_proj = function(cmd)
+  require 'code_runner'.setup {
+    filetype = nil,
+    project = {
+      [M.dir] = {
+        name = 'C Proj',
+        description = 'Project with cmakelists',
+        command = cmd,
+      },
+    },
+  }
+end
+
 require 'code_runner'.setup {
-  startinsert = true,
+  startinsert = false,
   filetype = {
     python = 'python -u',
     c = M.get_cmd_build(),
@@ -152,6 +217,79 @@ end
 M.rebuild_run = function()
   M.setup_ft { c = M.get_cmd_build_run(), }
   M.done()
+end
+
+-- 0: build and run
+-- 1: build
+-- 2: run
+M.run_mode = 0
+M.dir_bak = ''
+M.curname_bak = ''
+
+M.is_c_proj = function(run_mode)
+  local c_proj = nil
+  if vim.bo.ft == 'c' or vim.bo.ft == 'cpp' or vim.fn.expand '%:p:t' == 'CMakeLists.txt' then
+    local parent_p = require 'plenary.path':new(vim.api.nvim_buf_get_name(0)):parent()
+    local cmakelists = parent_p:joinpath 'CMakeLists.txt'
+    M.dir = parent_p.filename
+    if cmakelists:exists() then
+      local content = cmakelists:read()
+      M.projname = content:match 'set%(PROJECT_NAME ([%S]+)%)'
+      M.curname = content:match '${PROJECT_SOURCE_DIR}/([%w%p]+).c'
+      if M.projname == vim.fn.expand '%:p:t:r' or M.projname == 'common' then
+        c_proj = 1
+        if M.dir_bak ~= M.dir or M.curname_bak ~= M.curname or M.run_mode ~= run_mode then
+          M.dir_bak = M.dir
+          M.curname_bak = M.curname
+          M.run_mode = run_mode
+        end
+      end
+    end
+    if not c_proj then
+      M.run_mode = -1
+    end
+  end
+  return c_proj
+end
+
+M.run_proj = function()
+  if M.is_c_proj(2) then
+    M.setup_proj(M.get_cmd_run_proj())
+    M.done()
+  end
+end
+
+M.build_proj = function()
+  if M.is_c_proj(1) then
+    M.rebuild_en = nil
+    M.setup_proj(M.get_cmd_build_proj())
+    M.done()
+  end
+end
+
+M.rebuild_proj = function()
+  if M.is_c_proj(1) then
+    M.rebuild_en = 1
+    M.setup_proj(M.get_cmd_build_proj())
+    M.done()
+  end
+end
+
+M.build_run_proj = function()
+  if M.is_c_proj(0) then
+    M.rebuild_en = nil
+    M.setup_proj(M.get_cmd_build_run_proj())
+    M.done()
+  end
+end
+
+M.rebuild_run_proj = function()
+  if M.is_c_proj(0) then
+    M.build_run_proj()
+    M.rebuild_en = 1
+    M.setup_proj(M.get_cmd_build_run_proj())
+    M.done()
+  end
 end
 
 return M
--- start/lazy/lua/plugins/coderunner.lua
+++ start/lazy/lua/plugins/coderunner.lua
@@ -3,16 +3,24 @@ return {
   lazy = true,
   commit = '3c365982',
   keys = {
-    { '<leader>rb',        function() require 'config.coderunner'.build() end,       mode = { 'n', 'v', }, silent = true, desc = 'code runner build', },
-    { '<leader>rB',        function() require 'config.coderunner'.rebuild() end,     mode = { 'n', 'v', }, silent = true, desc = 'code runner rebuild', },
-    { '<leader>r<leader>', function() require 'config.coderunner'.run() end,         mode = { 'n', 'v', }, silent = true, desc = 'code runner run', },
-    { '<leader>rr',        function() require 'config.coderunner'.build_run() end,   mode = { 'n', 'v', }, silent = true, desc = 'code runner build_run', },
-    { '<leader>rR',        function() require 'config.coderunner'.rebuild_run() end, mode = { 'n', 'v', }, silent = true, desc = 'code runner rebuild_run', },
+    -- cur
+    { '<leader>rb',        function() require 'config.coderunner'.build() end,            mode = { 'n', 'v', }, silent = true, desc = 'code runner build', },
+    { '<leader>rB',        function() require 'config.coderunner'.rebuild() end,          mode = { 'n', 'v', }, silent = true, desc = 'code runner rebuild', },
+    { '<leader>r<leader>', function() require 'config.coderunner'.run() end,              mode = { 'n', 'v', }, silent = true, desc = 'code runner run', },
+    { '<leader>rr',        function() require 'config.coderunner'.build_run() end,        mode = { 'n', 'v', }, silent = true, desc = 'code runner build_run', },
+    { '<leader>rR',        function() require 'config.coderunner'.rebuild_run() end,      mode = { 'n', 'v', }, silent = true, desc = 'code runner rebuild_run', },
+
+    -- proj
+    { '<leader>rp',        function() require 'config.coderunner'.build_proj() end,       mode = { 'n', 'v', }, silent = true, desc = 'code runner build proj', },
+    { '<leader>rP',        function() require 'config.coderunner'.rebuild_proj() end,     mode = { 'n', 'v', }, silent = true, desc = 'code runner rebuild proj', },
+    { '<leader>r<cr>',     function() require 'config.coderunner'.run_proj() end,         mode = { 'n', 'v', }, silent = true, desc = 'code runner run proj', },
+    { '<leader>ro',        function() require 'config.coderunner'.build_run_proj() end,   mode = { 'n', 'v', }, silent = true, desc = 'code runner build_run proj', },
+    { '<leader>rO',        function() require 'config.coderunner'.rebuild_run_proj() end, mode = { 'n', 'v', }, silent = true, desc = 'code runner rebuild_run proj', },
 
     --c
-    { '<f9>',              function() require 'config.coderunner_c'.justbuild() end, mode = { 'n', 'v', }, silent = true, desc = 'code runner c to_make and justbuild', },
-    { '<c-f9>',            function() require 'config.coderunner_c'.rebuild() end,   mode = { 'n', 'v', }, silent = true, desc = 'code runner c to_make and rebuild', },
-    { '<c-f10>',           function() require 'config.coderunner_c'.to_cmake() end,  mode = { 'n', 'v', }, silent = true, desc = 'code runner c to_cmake', },
+    { '<f9>',              function() require 'config.coderunner_c'.justbuild() end,      mode = { 'n', 'v', }, silent = true, desc = 'code runner c to_make and justbuild', },
+    { '<c-f9>',            function() require 'config.coderunner_c'.rebuild() end,        mode = { 'n', 'v', }, silent = true, desc = 'code runner c to_make and rebuild', },
+    { '<c-f10>',           function() require 'config.coderunner_c'.to_cmake() end,       mode = { 'n', 'v', }, silent = true, desc = 'code runner c to_cmake', },
   },
   dependencies = {
     require 'plugins.projectroot',
