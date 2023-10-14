1762c755dfd7aca2157a9719e03b70e27f7674e3
peter-lyr <llydrp.ldp@gmail.com>
2023-10-15 01:57:15 +0800

    feat: markdown-preview mkdp_combine_preview = 1

--- start/lazy/lua/plugins/markdown_preview.lua
+++ start/lazy/lua/plugins/markdown_preview.lua
@@ -1,19 +1,27 @@
 return {
-  -- 'iamcco/markdown-preview.nvim',
-  -- lazy = true,
-  -- build = ":call mkdp#util#install()",
-  -- ft = {
-  --   'markdown',
-  -- },
-  -- keys = {
-  --   { '<f3><f3>',   '<cmd>MarkdownPreview<cr>',     mode = { 'v', 'n', }, silent = true, desc = 'MarkdownPreview', },
-  --   { '<f3><s-f3>', '<cmd>MarkdownPreviewStop<cr>', mode = { 'v', 'n', }, silent = true, desc = 'MarkdownPreviewStop', },
-  -- },
-  -- dependencies = {
-  --   require 'plugins.plenary',
-  -- },
-  -- init = function()
-  --   vim.g.mkdp_theme = 'dark'
-  --   vim.g.mkdp_auto_close = 0
-  -- end,
+  'iamcco/markdown-preview.nvim',
+  lazy = true,
+  build = ':call mkdp#util#install()',
+  ft = {
+    'markdown',
+  },
+  cmd = {
+    'MarkdownPreview',
+    'MarkdownPreviewStop',
+    'MarkdownPreviewToggle',
+  },
+  keys = {
+    { '<leader>m<leader>', '<cmd>MarkdownPreview<cr>',     mode = { 'v', 'n', }, silent = true, desc = 'MarkdownPreview', },
+    { '<leader>mq',        '<cmd>MarkdownPreviewStop<cr>', mode = { 'v', 'n', }, silent = true, desc = 'MarkdownPreviewStop', },
+  },
+  dependencies = {
+    require 'plugins.plenary',
+  },
+  init = function()
+    vim.g.mkdp_theme           = 'dark'
+    vim.g.mkdp_auto_close      = 0
+    vim.g.mkdp_auto_start      = 1
+    vim.g.mkdp_combine_preview = 1
+    require 'config.whichkey'.add { ['<leader>m'] = { name = 'MarkdownPreview', }, }
+  end,
 }
