3b203205c2beb3dc658864abd01364d8a5b6ff05
peter-lyr <llydrp.ldp@gmail.com>
2023-10-15 23:50:25 +0800

    fix: $png

--- opt/drag/lua/GetClipboardImage.ps1
+++ opt/drag/lua/GetClipboardImage.ps1
@@ -1,14 +1,11 @@
 # set-ExecutionPolicy RemoteSigned
-param($image_path, $sel_jpg)
+param($image_path, $png)
 Add-Type -AssemblyName System.Windows.Forms;
 if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {
   $imageObj = [System.Drawing.Bitmap][System.Windows.Forms.Clipboard]::GetDataObject().getimage();
-  if ($sel_jpg -eq "sel_jpg") {
+  if ($png -eq "png") {
     $imageObj.Save($image_path, [System.Drawing.Imaging.ImageFormat]::Jpeg);
   } else {
     $imageObj.Save($image_path, [System.Drawing.Imaging.ImageFormat]::Png);
   }
-  return 1
-} else {
-  return 0
 }
