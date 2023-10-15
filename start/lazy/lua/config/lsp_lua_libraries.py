474c58b7a945a185c9e1d72aaf2097030c02a844
peter-lyr <llydrp.ldp@gmail.com>
2023-10-15 23:46:52 +0800

    format: .

--- start/lazy/lua/config/lsp_lua_libraries.py
+++ start/lazy/lua/config/lsp_lua_libraries.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         os._exit(1)
     L = []
-    with open(sys.argv[1], 'wb') as f:
+    with open(sys.argv[1], "wb") as f:
         for i, runtime in enumerate(sys.argv[2:]):
             for root, dirs, files in os.walk(runtime):
                 for dir in dirs:
-                    if dir == 'lua':
-                        l = os.path.join(root, dir).encode('utf-8')
+                    if dir == "lua":
+                        l = os.path.join(root, dir).encode("utf-8")
                         if l not in L:
                             L.append(l)
-                            f.write(l + b'\n')
+                            f.write(l + b"\n")
