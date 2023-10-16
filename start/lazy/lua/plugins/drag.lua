e70784973989ed6541ef0e532781d45294e5eaec
peter-lyr <llydrp.ldp@gmail.com>
2023-10-16 16:04:34 +0800

    other: no update docs

--- start/lazy/lua/plugins/drag.lua
+++ start/lazy/lua/plugins/drag.lua
@@ -10,8 +10,6 @@ return {
     { '<leader>mU',  function() require 'drag_images'.update 'cwd' end, mode = { 'n', 'v', }, silent = true, desc = 'markdown images update cwd', },
     { '<leader>mv',  function() require 'drag_images'.paste 'jpg' end,  mode = { 'n', 'v', }, silent = true, desc = 'markdown paste jpg image', },
     { '<leader>mV',  function() require 'drag_images'.paste 'png' end,  mode = { 'n', 'v', }, silent = true, desc = 'markdown paste png image', },
-    { '<leader>mdu', function() require 'drag_docs'.update 'cur' end,   mode = { 'n', 'v', }, silent = true, desc = 'markdown docs update cur', },
-    { '<leader>mdU', function() require 'drag_docs'.update 'cwd' end,   mode = { 'n', 'v', }, silent = true, desc = 'markdown docs update cwd', },
   },
   dependencies = {
     require 'plugins.plenary',
