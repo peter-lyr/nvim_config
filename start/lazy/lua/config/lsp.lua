5d036054a8c5f1f509176f2e26ed037974bc9edf
peter-lyr <llydrp.ldp@gmail.com>
2023-10-17 13:48:39 +0800

    fix: lsp K

--- start/lazy/lua/config/lsp.lua
+++ start/lazy/lua/config/lsp.lua
@@ -245,6 +245,10 @@ vim.g.lsp_au_lspattach = vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    local opts = function(desc)
+      return { buffer = ev.buf, desc = desc, }
+    end
+    vim.keymap.set({ 'n', 'v', }, 'K', vim.lsp.buf.definition, opts 'vim.lsp.buf.definition')
   end,
 })
 
