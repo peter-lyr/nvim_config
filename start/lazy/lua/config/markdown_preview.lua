dff71623868c1195f69bcdc2662f1451e488220a
peter-lyr <llydrp.ldp@gmail.com>
2023-10-17 11:40:47 +0800

    fix: mkdp

--- start/lazy/lua/config/markdown_preview.lua
+++ start/lazy/lua/config/markdown_preview.lua
@@ -1,18 +1,18 @@
 local M = {}
 
 M.start = function()
-  vim.cmd 'MarkdownPreview'
+  pcall(vim.cmd, 'MarkdownPreview')
 end
 
 M.restart = function()
   vim.cmd 'MarkdownPreviewStop'
   vim.fn.timer_start(20, function()
-    vim.cmd 'MarkdownPreview'
+    pcall(vim.cmd, 'MarkdownPreview')
   end)
 end
 
 M.stop = function()
-  vim.cmd 'MarkdownPreviewStop'
+  pcall(vim.cmd, 'MarkdownPreviewStop')
 end
 
 return M
