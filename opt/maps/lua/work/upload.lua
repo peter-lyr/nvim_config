eff5279c80e741ad0e871cfaf3b5c67d93b87b1c
peter-lyr <llydrp.ldp@gmail.com>
2023-10-19 11:04:22 +0800

    fix: upload

--- opt/maps/lua/work/upload.lua
+++ opt/maps/lua/work/upload.lua
@@ -6,7 +6,7 @@ M.upload = function(file)
   if not file then
     file = vim.api.nvim_buf_get_name(0)
   end
-  vim.cmd(string.format([[silent !start /b /min cmd /c "C:\Program Files\FileServ\upload.bat "%s""]], file))
+  vim.cmd(string.format([[silent !start /b /min cmd /c "upload.bat "%s""]], file))
 end
 
 return M
