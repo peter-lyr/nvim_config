cc56a06034b2767375e36008f96210c40c2f429e
peter-lyr <llydrp.ldp@gmail.com>
2023-10-20 12:33:21 +0800

    feat: tabline leader q

--- start/lazy/lua/plugins/tabline.lua
+++ start/lazy/lua/plugins/tabline.lua
@@ -11,26 +11,22 @@ return {
     'peter-lyr/vim-bbye',
   },
   keys = {
-    { '<c-h>',       function() require 'tabline'.b_prev_buf() end,                      mode = { 'n', 'v', }, silent = true, desc = 'tabline b prev buffer', },
-    { '<c-l>',       function() require 'tabline'.b_next_buf() end,                      mode = { 'n', 'v', }, silent = true, desc = 'tabline b next buffer', },
-    { '<c-s-h>',     function() require 'tabline'.bd_prev_buf() end,                     mode = { 'n', 'v', }, silent = true, desc = 'tabline bd prev buffer', },
-    { '<c-s-l>',     function() require 'tabline'.bd_next_buf() end,                     mode = { 'n', 'v', }, silent = true, desc = 'tabline bd next buffer', },
-    { '<c-q><c-w>',  function() require 'tabline'.only_cur_buffer() end,                 mode = { 'n', 'v', }, silent = true, desc = 'tabline only cur buffer', },
-    { '<c-q>w',      function() require 'tabline'.only_cur_buffer() end,                 mode = { 'n', 'v', }, silent = true, desc = 'tabline only cur buffer', },
-    { '<c-q><c-t>',  function() require 'tabline'.restore_hidden_tabs() end,             mode = { 'n', 'v', }, silent = true, desc = 'tabline restore hidden tabs', },
-    { '<c-q>t',      function() require 'tabline'.restore_hidden_tabs() end,             mode = { 'n', 'v', }, silent = true, desc = 'tabline restore hidden tabs', },
-    { '<c-q><c-o>',  function() require 'tabline'.append_one_proj_right_down() end,      mode = { 'n', 'v', }, silent = true, desc = 'tabline append one proj right down', },
-    { '<c-q>o',      function() require 'tabline'.append_one_proj_right_down() end,      mode = { 'n', 'v', }, silent = true, desc = 'tabline append one proj right down', },
-    { '<c-q><c-n>',  function() require 'tabline'.append_one_proj_new_tab() end,         mode = { 'n', 'v', }, silent = true, desc = 'tabline append one proj new tab', },
-    { '<c-q>n',      function() require 'tabline'.append_one_proj_new_tab() end,         mode = { 'n', 'v', }, silent = true, desc = 'tabline append one proj new tab', },
-    { '<c-q><c-m>',  function() require 'tabline'.append_one_proj_new_tab_no_dupl() end, mode = { 'n', 'v', }, silent = true, desc = 'tabline append one proj new tab no dupl', },
-    { '<c-q>m',      function() require 'tabline'.append_one_proj_new_tab_no_dupl() end, mode = { 'n', 'v', }, silent = true, desc = 'tabline append one proj new tab no dupl', },
-    { '<c-q><c-bs>', function() require 'tabline'.simple_statusline_toggle() end,        mode = { 'n', 'v', }, silent = true, desc = 'tabline simple statusline toggle', },
-    { '<leader>at',  function() require 'tabline'.only_tabs_toggle() end,                mode = { 'n', 'v', }, silent = true, desc = 'tabline show only tabs toggle', },
+    { '<c-h>',             function() require 'tabline'.b_prev_buf() end,                      mode = { 'n', 'v', }, silent = true, desc = 'tabline b prev buffer', },
+    { '<c-l>',             function() require 'tabline'.b_next_buf() end,                      mode = { 'n', 'v', }, silent = true, desc = 'tabline b next buffer', },
+    { '<c-s-h>',           function() require 'tabline'.bd_prev_buf() end,                     mode = { 'n', 'v', }, silent = true, desc = 'tabline bd prev buffer', },
+    { '<c-s-l>',           function() require 'tabline'.bd_next_buf() end,                     mode = { 'n', 'v', }, silent = true, desc = 'tabline bd next buffer', },
+    { '<leader>qw',        function() require 'tabline'.only_cur_buffer() end,                 mode = { 'n', 'v', }, silent = true, desc = 'tabline only cur buffer', },
+    { '<leader>qt',        function() require 'tabline'.restore_hidden_tabs() end,             mode = { 'n', 'v', }, silent = true, desc = 'tabline restore hidden tabs', },
+    { '<leader>qo',        function() require 'tabline'.append_one_proj_right_down() end,      mode = { 'n', 'v', }, silent = true, desc = 'tabline append one proj right down', },
+    { '<leader>qn',        function() require 'tabline'.append_one_proj_new_tab() end,         mode = { 'n', 'v', }, silent = true, desc = 'tabline append one proj new tab', },
+    { '<leader>qm',        function() require 'tabline'.append_one_proj_new_tab_no_dupl() end, mode = { 'n', 'v', }, silent = true, desc = 'tabline append one proj new tab no dupl', },
+    { '<leader>q<leader>', function() require 'tabline'.simple_statusline_toggle() end,        mode = { 'n', 'v', }, silent = true, desc = 'tabline simple statusline toggle', },
+    { '<leader>qq',        function() require 'tabline'.only_tabs_toggle() end,                mode = { 'n', 'v', }, silent = true, desc = 'tabline show only tabs toggle', },
   },
   init = function()
     vim.opt.tabline     = ' ' .. vim.loop.cwd()
     vim.opt.showtabline = 2
+    require 'config.whichkey'.add { ['<leader>q'] = { name = 'Tabline', }, }
   end,
   config = function()
     require 'tabline'
