2a96fd97efb247e9dc3e486706cc1530f80b6a8d
peter-lyr <llydrp.ldp@gmail.com>
2023-10-20 11:42:14 +0800

    fix: projectroot au cd

--- start/lazy/lua/plugins/projectroot.lua
+++ start/lazy/lua/plugins/projectroot.lua
@@ -5,5 +5,10 @@ return {
     vim.g.rootmarkers = {
       '.git',
     }
+    vim.api.nvim_create_autocmd({ 'BufEnter', }, {
+      callback = function()
+        pcall(vim.call, 'ProjectRootCD')
+      end,
+    })
   end,
 }
