f5271eac0054d0f2cf5505784d5249a1d1127729
peter-lyr <llydrp.ldp@gmail.com>
2023-10-21 23:59:43 +0800

    style: cmp

---
+++ start/lazy/lua/config/cmp_snip.lua
@@ -0,0 +1,15 @@
+local M = {}
+local B = require 'my_base'
+M.source = debug.getinfo(1)['source']
+package.loaded[B.get_loaded(M.source)] = nil
+--------------------------------------------
+
+require 'luasnip.loaders.from_vscode'.lazy_load()
+require 'luasnip.loaders.from_snipmate'.lazy_load { paths = { B.get_opt_dir 'snippets', }, }
+
+require 'luasnip'.config.setup {
+  history = true,
+  delete_check_events = 'TextChanged',
+}
+
+return M
--- start/lazy/lua/plugins/cmp.lua
+++ start/lazy/lua/plugins/cmp.lua
@@ -12,27 +12,13 @@ return {
     {
       'saadparwaiz1/cmp_luasnip',
       dependencies = {
-        -- snippets
         'L3MON4D3/LuaSnip',
         dependencies = {
           'rafamadriz/friendly-snippets',
-          dependencies = {
-            require 'plugins.plenary',
-          },
-          config = function()
-            require 'luasnip.loaders.from_vscode'.lazy_load()
-            require 'luasnip.loaders.from_snipmate'.lazy_load {
-              paths = {
-                require 'plenary.path':new(vim.g.pack_path):joinpath('nvim_config', 'opt', 'snippets').filename,
-              },
-            }
-            require 'luasnip'.config.setup {}
-          end,
         },
-        opts = {
-          history = true,
-          delete_check_events = 'TextChanged',
-        },
+        config = function()
+          require 'config.cmp_snip'
+        end,
       },
     },
   },
