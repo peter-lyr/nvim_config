f44ff847fbfeca5b38cd48ec0d5d0bc09b4f80e1
peter-lyr <llydrp.ldp@gmail.com>
2023-10-22 01:06:15 +0800

    style: marks

---
+++ start/lazy/lua/config/marks.lua
@@ -0,0 +1,30 @@
+local M = {}
+local B = require 'my_base'
+M.source = debug.getinfo(1)['source']
+package.loaded[B.get_loaded(M.source)] = nil
+--------------------------------------------
+
+require 'marks'.setup {
+  default_mappings = false,
+  builtin_marks = { '.', '<', '>', '^', },
+  cyclic = false,
+  force_write_shada = false,
+  refresh_interval = 250,
+  sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20, },
+  excluded_filetypes = {
+    'NvimTree',
+    'fugitive',
+    'help',
+  },
+  mappings = {
+    toggle = 'mm',
+    set_next = 'mn',
+    delete_line = 'md',
+    delete_buf = 'mx',
+    next = 'ms',
+    prev = 'mw',
+    annotate = 'ma',
+  },
+}
+
+return M
--- start/lazy/lua/plugins/marks.lua
+++ start/lazy/lua/plugins/marks.lua
@@ -3,30 +3,10 @@ return {
   'peter-lyr/marks.nvim',
   lazy = true,
   event = { 'BufReadPost', 'BufNewFile', },
+  keys = {
+    { 'mq', '<cmd>MarksQFListBuf<cr>', mode = { 'n', 'v', }, silent = true, desc = 'MarksQFListBuf', },
+  },
   config = function()
-    require 'marks'.setup {
-      default_mappings = false,
-      builtin_marks = { '.', '<', '>', '^', },
-      cyclic = false,
-      force_write_shada = false,
-      refresh_interval = 250,
-      sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20, },
-      excluded_filetypes = {
-        'NvimTree',
-        'fugitive',
-        'help',
-      },
-      mappings = {
-        toggle = 'mm',
-        set_next = 'm,',
-        delete_line = 'md',
-        delete_buf = 'mx',
-        next = 'ms',
-        prev = 'mw',
-        annotate = 'ma',
-      },
-    }
-    vim.keymap.set({ 'n', 'v', }, 'mq', '<cmd>MarksQFListBuf<cr>', { desc = 'MarksQFListBuf', })
-    vim.keymap.set({ 'n', 'v', }, 'ma', '<cmd>MarksQFListAll<cr>', { desc = 'MarksQFListAll', })
+    require 'config.marks'
   end,
 }
