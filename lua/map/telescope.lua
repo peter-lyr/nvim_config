ecd07d17c1671ae3e9095669449a5795832308f8
peter-lyr <llydrp.ldp@gmail.com>
2023-10-25 10:56:33 +0800

    .

--- lua/map/telescope.lua
+++ lua/map/telescope.lua
@@ -7,6 +7,7 @@ M.config = B.rep_map_to_config(M.loaded)
 --------------------------------------------
 
 B.load_require_common()
+
 B.load_require 'nvim-lua/plenary.nvim'
 B.load_require 'nvim-tree/nvim-web-devicons'
 
