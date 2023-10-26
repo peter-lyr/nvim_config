74d2d255cae800d511931d54105d055e435eecdc
peter-lyr <llydrp.ldp@gmail.com>
2023-10-26 14:21:09 +0800

    test: fix

--- lua/map/test.lua
+++ lua/map/test.lua
@@ -13,22 +13,22 @@ B.load_require 'dstein64/vim-startuptime'
 
 B.map_set_lua(M.config)
 
-B.map('<c-s-f4>1', M.config, 'execute_output', { 'mes', })
-B.map('<c-s-f4>2', M.config, 'execute_output', { 'scriptnames', })
-B.map('<c-s-f4>3', M.config, 'execute_output', { '!dir', })
-B.map('<c-s-f4>4', M.config, 'execute_output', { 'set rtp', })
+B.map('<c-s-f4>1', 'execute_output', { 'mes', })
+B.map('<c-s-f4>2', 'execute_output', { 'scriptnames', })
+B.map('<c-s-f4>3', 'execute_output', { '!dir', })
+B.map('<c-s-f4>4', 'execute_output', { 'set rtp', })
 
-B.map('<c-s-f4><del>', M.config, 'delete_whichkeys_txt', {})
-B.map('<c-s-f4><f4>', M.config, 'startuptime', {})
-B.map('<c-s-f4>s', M.config, 'start_new_nvim_qt', {})
-B.map('<c-s-f4>r', M.config, 'restart_nvim_qt', {})
-B.map('<c-s-f4>q', M.config, 'quit_nvim_qt', {})
+B.map('<c-s-f4><del>', 'delete_whichkeys_txt', {})
+B.map('<c-s-f4><f4>', 'startuptime', {})
+B.map('<c-s-f4>s', 'start_new_nvim_qt', {})
+B.map('<c-s-f4>r', 'restart_nvim_qt', {})
+B.map('<c-s-f4>q', 'quit_nvim_qt', {})
 
-B.map('<c-s-f4><f3>', M.config, 'source_lua', {})
+B.map('<c-s-f4><f3>', 'source_lua', {})
 
 B.map_set_opts { silent = false, }
 
-B.map('<c-s-f4>t', M.config, 'type_execute_output', {})
+B.map('<c-s-f4>t', 'type_execute_output', {})
 
 B.map_reset_opts()
 
