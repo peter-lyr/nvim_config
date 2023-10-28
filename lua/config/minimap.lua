7fbbb6051341a2075903230c649d67d71b5c2be7
peter-lyr <llydrp.ldp@gmail.com>
2023-10-28 23:44:12 +0800

    minimap

--- lua/config/minimap.lua
+++ lua/config/minimap.lua
@@ -56,4 +56,48 @@ function M.toggle_focus()
   minimap.toggle_focus()
 end
 
+function M.cr(cnt)
+  for _ = 1, cnt do
+    vim.cmd [[call feedkeys("\<cr>")]]
+  end
+end
+
+function M.esc(cnt)
+  for _ = 1, cnt do
+    vim.cmd [[call feedkeys("\<esc>")]]
+  end
+end
+
+function M.up()
+  vim.cmd [[call feedkeys("\<up>")]]
+end
+
+function M.down()
+  vim.cmd [[call feedkeys("\<down>")]]
+end
+
+B.aucmd(M.source, 'BufEnter', 'BufEnter', {
+  callback = function(ev)
+    if vim.api.nvim_buf_get_option(ev.buf, 'filetype') == 'minimap' then
+      vim.cmd 'setlocal signcolumn=no'
+      vim.fn.timer_start(20, function()
+        vim.keymap.set({ 'n', }, '<MiddleMouse>', function() M.esc(1) end, { buffer = ev.buf, desc = 'MiniMap esc', })
+        vim.keymap.set({ 'v', }, '<MiddleMouse>', function() M.esc(2) end, { buffer = ev.buf, desc = 'MiniMap esc', })
+        vim.keymap.set({ 'n', }, 'q', function() M.esc(1) end, { buffer = ev.buf, desc = 'MiniMap esc', })
+        vim.keymap.set({ 'v', }, 'q', function() M.esc(2) end, { buffer = ev.buf, desc = 'MiniMap esc', })
+        vim.keymap.set({ 'n', }, '`', function() M.esc(1) end, { buffer = ev.buf, desc = 'MiniMap esc', })
+        vim.keymap.set({ 'v', }, '`', function() M.esc(2) end, { buffer = ev.buf, desc = 'MiniMap esc', })
+        vim.keymap.set({ 'n', }, '<2-LeftMouse>', function() M.cr(1) end, { buffer = ev.buf, desc = 'MiniMap cr', })
+        vim.keymap.set({ 'v', }, '<2-LeftMouse>', function() M.cr(2) end, { buffer = ev.buf, desc = 'MiniMap cr', })
+        vim.keymap.set({ 'n', }, '<tab>', function() M.cr(1) end, { buffer = ev.buf, desc = 'MiniMap cr', })
+        vim.keymap.set({ 'v', }, '<tab>', function() M.cr(2) end, { buffer = ev.buf, desc = 'MiniMap cr', })
+        vim.keymap.set({ 'n', }, 'a', function() M.cr(1) end, { buffer = ev.buf, desc = 'MiniMap cr', })
+        vim.keymap.set({ 'v', }, 'a', function() M.cr(2) end, { buffer = ev.buf, desc = 'MiniMap cr', })
+        vim.keymap.set({ 'n', 'v', }, '<ScrollWheelUp>', function() M.up() end, { buffer = ev.buf, desc = 'MiniMap up', })
+        vim.keymap.set({ 'n', 'v', }, '<ScrollWheelDown>', function() M.down() end, { buffer = ev.buf, desc = 'MiniMap down', })
+      end)
+    end
+  end,
+})
+
 return M
