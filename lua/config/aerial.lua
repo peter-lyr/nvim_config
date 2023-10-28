28d4049591bfb17d003242564f9fbefbe4e45a90
peter-lyr <llydrp.ldp@gmail.com>
2023-10-29 02:32:24 +0800

    aerial

---
+++ lua/config/aerial.lua
@@ -0,0 +1,67 @@
+local M = {}
+local B = require 'my_base'
+M.source = B.get_source(debug.getinfo(1)['source'])
+M.loaded = B.get_loaded(M.source)
+-- package.loaded[M.loaded] = nil
+--------------------------------------------
+
+M.width = 25
+
+M.default_opts = {
+  layout = {
+    max_width = M.width,
+    min_width = M.width,
+    preserve_equality = nil,
+  },
+  keymaps = {
+    ['<C-s>'] = false,
+    ['<C-j>'] = false,
+    ['<C-k>'] = false,
+    ['?'] = 'actions.show_help',
+    ['a'] = 'actions.jump',
+    ['<2-LeftMouse>'] = 'actions.jump',
+    ['<MiddleMouse>'] = 'actions.close',
+    ['<C-x>'] = 'actions.jump_split',
+    ['<tab>'] = 'actions.scroll',
+    ['o'] = 'actions.tree_toggle',
+    ['O'] = 'actions.tree_toggle_recursive',
+    ['l'] = 'actions.tree_open',
+    ['L'] = 'actions.tree_open_recursive',
+    ['h'] = 'actions.tree_close',
+    ['H'] = 'actions.tree_close_recursive',
+    ['r'] = 'actions.tree_increase_fold_level',
+    ['R'] = 'actions.tree_open_all',
+    ['p'] = 'actions.tree_decrease_fold_level',
+    ['P'] = 'actions.tree_close_all',
+  },
+  filter_kind = false,
+  backends = { 'markdown', 'lsp', 'treesitter', 'man', },
+  -- backends = { "lsp", },
+  post_jump_cmd = [[norm zz]],
+  close_automatic_events = {},
+  close_on_select = false,
+  float = {
+    relative = 'editor',
+  },
+}
+
+require 'aerial'.setup(M.default_opts)
+
+function M.setup(opts)
+  opts = vim.tbl_deep_extend('force', M.default_opts, opts)
+  require 'aerial'.setup(opts)
+end
+
+M.opened = nil
+
+function M.open()
+  M.opened = 1
+  vim.cmd 'AerialOpen right'
+end
+
+function M.close()
+  M.opened = 1
+  vim.cmd 'AerialCloseAll'
+end
+
+return M
---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,19 @@
+local S = require 'startup'
+
+return {
+  'stevearc/aerial.nvim',
+  lazy = true,
+  event = { 'LspAttach', },
+  keys = {
+    { ']a', '<cmd>AerialNext<cr>', mode = { 'n', 'v', }, silent = true, desc = 'AerialNext', },
+    { '[a', '<cmd>AerialPrev<cr>', mode = { 'n', 'v', }, silent = true, desc = 'AerialPrev', },
+  },
+  init = function()
+    if not S.enable then
+      require 'my_simple'.add_whichkey('<leader>a', plugin, 'Aerial')
+    end
+  end,
+  config = function()
+    require 'map.aerial'
+  end,
+}
