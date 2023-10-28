913f4dcba9ac6c9f7ab4db1fbc132f1ef55a6a5c
peter-lyr <llydrp.ldp@gmail.com>
2023-10-29 02:24:38 +0800

    keys <a-s-h>

--- lua/plugins/window.lua
+++ lua/plugins/window.lua
@@ -6,6 +6,12 @@ return {
   name = plugin,
   dir = '',
   lazy = true,
+  keys = {
+    { '<a-s-h>', function() require 'config.window'.height_less() end, mode = { 'n', 'v', }, silent = true, desc = 'Window height_less', },
+    { '<a-s-l>', function() require 'config.window'.height_more() end, mode = { 'n', 'v', }, silent = true, desc = 'Window height_more', },
+    { '<a-s-j>', function() require 'config.window'.width_less() end,  mode = { 'n', 'v', }, silent = true, desc = 'Window width_less', },
+    { '<a-s-k>', function() require 'config.window'.width_more() end,  mode = { 'n', 'v', }, silent = true, desc = 'Window width_more', },
+  },
   init = function()
     if not S.enable then
       require 'my_simple'.add_whichkey('<leader>w', plugin, 'Window')
