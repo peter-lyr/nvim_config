f30f091a619a5787c92ba6e17d7e9166198010f9
peter-lyr <llydrp.ldp@gmail.com>
2023-10-29 23:50:22 +0800

    fix: M.get_loaded_valid_bufs()

--- lua/my_base_funcs.lua
+++ lua/my_base_funcs.lua
@@ -1,6 +1,11 @@
 local M = {}
+local B = require 'my_base'
+M.source = B.get_source(debug.getinfo(1)['source'])
+M.loaded = B.get_loaded(M.source)
+-- package.loaded[M.loaded] = nil
+--------------------------------------------
 
-function B.get_loaded_valid_bufs()
+function M.get_loaded_valid_bufs()
   local files = {}
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
     if B.is_buf_loaded_valid(buf) then
