1263dff996378063bfa39586bc73abd84b66399c
peter-lyr <llydrp.ldp@gmail.com>
2023-11-03 09:35:32 +0800

    other: add comment on todo-comments alt

--- lua/config/editor_todo.lua
+++ lua/config/editor_todo.lua
@@ -5,7 +5,22 @@ M.loaded = B.get_loaded(M.source)
 -- package.loaded[M.loaded] = nil
 --------------------------------------------
 
-require 'todo-comments'.setup {}
+require 'todo-comments'.setup {
+  -- keywords = {
+  --   FIX = {
+  --     icon = ' ', -- icon used for the sign, and in search results
+  --     color = 'error', -- can be a hex color, or a named color (see below)
+  --     alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE', }, -- a set of other keywords that all map to this FIX keywords
+  --     -- signs = false, -- configure signs for some keywords individually
+  --   },
+  --   TODO = { icon = ' ', color = 'info', },
+  --   HACK = { icon = ' ', color = 'warning', },
+  --   WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX', }, },
+  --   PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE', }, },
+  --   NOTE = { icon = ' ', color = 'hint', alt = { 'INFO', }, },
+  --   TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED', }, },
+  -- },
+}
 require 'map.sidepanel_quickfix'
 
 function M.TodoQuickFix()
