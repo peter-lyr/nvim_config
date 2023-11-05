9fb6f120f59e11a3e7fa20f1b7a2f13223fc6cb1
peter-lyr <llydrp.ldp@gmail.com>
2023-11-05 23:53:54 +0800

    <a-t>

--- lua/map/my_test.lua
+++ lua/map/my_test.lua
@@ -15,65 +15,65 @@ B.map_set_lua(M.config)
 
 -------
 
-B.map('<leader><f4>1', 'execute_output', { 'mes', })
-B.map('<leader><f4>2', 'execute_output', { 'scriptnames', })
-B.map('<leader><f4>3', 'execute_output', { '!dir', })
-B.map('<leader><f4>4', 'execute_output', { 'set rtp', })
-B.map('<leader><f4>5', 'execute_output', { 'ls!', })
+B.map('<a-t>1', 'execute_output', { 'mes', })
+B.map('<a-t>2', 'execute_output', { 'scriptnames', })
+B.map('<a-t>3', 'execute_output', { '!dir', })
+B.map('<a-t>4', 'execute_output', { 'set rtp', })
+B.map('<a-t>5', 'execute_output', { 'ls!', })
 
 -------
 
-B.map('<leader><f4><del>', 'delete_whichkeys_txt', {})
-B.map('<leader><f4><f4>', 'startuptime', {})
-B.map('<leader><f4><s-f4>', 'startuptime', { '--no-sort', })
-B.map('<leader><f4>s', 'start_new_nvim_qt', {})
-B.map('<leader><f4>r', 'restart_nvim_qt', {})
-B.map('<leader><f4>q', 'quit_nvim_qt', {})
+B.map('<a-t><del>', 'delete_whichkeys_txt', {})
+B.map('<a-t><f4>', 'startuptime', {})
+B.map('<a-t><s-f4>', 'startuptime', { '--no-sort', })
+B.map('<a-t>s', 'start_new_nvim_qt', {})
+B.map('<a-t>r', 'restart_nvim_qt', {})
+B.map('<a-t>q', 'quit_nvim_qt', {})
 
 -------
 
-B.map('<leader><f4><c-f1>', 'terminal_cmd', {})
-B.map('<leader><f4><c-f2>', 'terminal_ipython', {})
-B.map('<leader><f4><c-f3>', 'terminal_bash', {})
-B.map('<leader><f4><c-f4>', 'terminal_powershell', {})
+B.map('<a-t><c-f1>', 'terminal_cmd', {})
+B.map('<a-t><c-f2>', 'terminal_ipython', {})
+B.map('<a-t><c-f3>', 'terminal_bash', {})
+B.map('<a-t><c-f4>', 'terminal_powershell', {})
 
-B.map('<leader><f4><s-f1>', 'terminal_outside_cmd', {})
-B.map('<leader><f4><s-f2>', 'terminal_outside_ipython', {})
-B.map('<leader><f4><s-f3>', 'terminal_outside_bash', {})
-B.map('<leader><f4><s-f4>', 'terminal_outside_powershell', {})
-
--------
-
-B.map('<leader><f4>;', 'asyncrun', {})
-B.map('<leader><f4><c-del>', 'asyncstop', {})
+B.map('<a-t><s-f1>', 'terminal_outside_cmd', {})
+B.map('<a-t><s-f2>', 'terminal_outside_ipython', {})
+B.map('<a-t><s-f3>', 'terminal_outside_bash', {})
+B.map('<a-t><s-f4>', 'terminal_outside_powershell', {})
 
 -------
 
-B.map('<leader><f4>l', 'lazy', {})
-B.map('<leader><f4>m', 'mason', {})
+B.map('<a-t>;', 'asyncrun', {})
+B.map('<a-t><c-del>', 'asyncstop', {})
 
 -------
 
-B.map('<leader><f4><f3>', 'source_lua', {})
+B.map('<a-t>l', 'lazy', {})
+B.map('<a-t>m', 'mason', {})
+
+-------
+
+B.map('<a-t><f3>', 'source_lua', {})
 
 --------
 
-B.map('<leader><f4>gc', 'git_clone', {})
+B.map('<a-t>gc', 'git_clone', {})
 
 --------
 
-B.map('<leader><f4>ot', 'open_stdpath_temp', {})
-B.map('<leader><f4>oc', 'open_stdpath_config', {})
+B.map('<a-t>ot', 'open_stdpath_temp', {})
+B.map('<a-t>oc', 'open_stdpath_config', {})
 
 ------
 
 B.map_set_opts { silent = false, }
 
-B.map('<leader><f4>t', 'type_execute_output', {})
+B.map('<a-t>t', 'type_execute_output', {})
 
 B.map_reset_opts()
 
-B.register_whichkey('<leader><f4>o', 'open')
+B.register_whichkey('<a-t>o', 'open')
 
 B.merge_whichkeys()
 
