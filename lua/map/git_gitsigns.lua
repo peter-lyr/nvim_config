99352d2ec53f839033cc61a79d4b75dd29e256bd
peter-lyr <llydrp.ldp@gmail.com>
2023-11-05 15:32:54 +0800

    gitsigns setup

--- lua/map/git_gitsigns.lua
+++ lua/map/git_gitsigns.lua
@@ -59,11 +59,7 @@ vim.keymap.set('n', '<leader>k', function()
 end, { expr = true, desc = 'Gitsigns prev_hunk', })
 
 require 'gitsigns'.setup {
-  numhl         = true,
-  linehl        = false,
-  word_diff     = true,
-
-  signs         = {
+  signs                        = {
     add = { text = '+', },
     change = { text = '~', },
     delete = { text = '_', },
@@ -71,7 +67,38 @@ require 'gitsigns'.setup {
     changedelete = { text = '', },
     untracked = { text = '?', },
   },
-  sign_priority = 100,
+  signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir                 = {
+    follow_files = true,
+  },
+  attach_to_untracked          = true,
+  current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts      = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+    virt_text_priority = 100,
+  },
+  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+  sign_priority                = 100,
+  update_debounce              = 100,
+  status_formatter             = nil,   -- Use default
+  max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+  preview_config               = {
+    -- Options passed to nvim_open_win
+    border = 'single',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1,
+  },
+  yadm                         = {
+    enable = false,
+  },
 }
 
 return M
