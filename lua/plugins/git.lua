8ea0e6f296a24d1663cfd9aace95c701067c70de
peter-lyr <llydrp.ldp@gmail.com>
2023-11-05 15:56:18 +0800

    fix: peter-lyr/gitsigns.nvim diffthis \r

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,9 +2,9 @@ local S = require 'startup'
 
 return {
   {
-    'lewis6991/gitsigns.nvim',
+    -- 'lewis6991/gitsigns.nvim',
+    'peter-lyr/gitsigns.nvim',
     lazy = true,
-    -- commit = '79127db3b127f5d125f35e0d44ba60715edf2842',
     event = { 'BufReadPre', 'BufNewFile', },
     keys = {
       { 'ig',        ':<C-U>Gitsigns select_hunk<CR>', mode = { 'o', 'x', }, silent = true, desc = 'Gitsigns select_hunk', },
