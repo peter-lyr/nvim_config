db9775f9b0b3a0733537fa2864533e66d23705eb
peter-lyr <llydrp.ldp@gmail.com>
2023-11-16 19:46:17 +0800

    vim.opt.equalalways   = false

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -39,3 +39,4 @@ vim.opt.list          = true
 vim.opt.shada         = [[!,'1000,<500,s10000,h]]
 vim.opt.laststatus    = 3
 vim.opt.statusline    = [[%f %h%m%r%=%<%-14.(%l,%c%V%) %P]]
+vim.opt.equalalways   = false
