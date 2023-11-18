bec113bed65d78497e977b705019ebd7aa00453e
peter-lyr <llydrp.ldp@gmail.com>
2023-11-18 14:46:51 +0800

    local author, repo = string.match(vim.fn.input('author/repo to clone: ', 'peter-lyr/2023'), '(.+)/(.+)')

--- lua/config/my_test_useful.lua
+++ lua/config/my_test_useful.lua
@@ -41,8 +41,7 @@ end
 
 function M.git_clone()
   B.ui_sel(B.get_file_dirs(vim.api.nvim_buf_get_name(0)), 'git clone sel a dir', function(proj)
-    local author = vim.fn.input('repo name to clone: ', 'peter-lyr')
-    local repo = vim.fn.input('repo name to clone: ', '2023')
+    local author, repo = string.match(vim.fn.input('author/repo to clone: ', 'peter-lyr/2023'), '(.+)/(.+)')
     if B.is(author) and B.is(repo) then
       B.system_run('start', [[cd %s & git clone git@github.com:%s/%s.git]], proj, author, repo)
     end
