218a5a1b03b59d0bb23b5acab99f755820cfebb5
peter-lyr <llydrp.ldp@gmail.com>
2023-11-22 13:49:37 +0800

    copy_md move_sel

--- lua/config/sidepanel_nvimtree_map_sel.lua
+++ lua/config/sidepanel_nvimtree_map_sel.lua
@@ -113,6 +113,10 @@ function M.move_sel(node)
       else
         local fname = M.get_fname_tail(absolute_path)
         fname = string.format('%s\\%s', dtarget, fname)
+        local ext = string.match(fname, '%.([^.]+)$')
+        if vim.tbl_contains(M.markdowns_fts, ext) == true then
+          require 'config.my_drag_images'.copy_md(vim.fn['ProjectRootGet'](absolute_path), absolute_path, vim.fn['ProjectRootGet'](dtarget), dtarget)
+        end
         if require 'plenary.path':new(fname):exists() then
           vim.cmd 'redraw'
           local fname_new = vim.fn.input(absolute_path .. ' ->\nExisted! Rename? ', fname)
