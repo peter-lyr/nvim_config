d9dbda56c941026f6aedc8e7b4da0fa5f3f596d5
peter-lyr <llydrp.ldp@gmail.com>
2023-11-25 19:59:42 +0800

    M.get_str_num(str, hex)

--- lua/config/my_cal.lua
+++ lua/config/my_cal.lua
@@ -6,35 +6,41 @@ M.loaded = B.get_loaded(M.source)
 M.lua = string.match(M.loaded, '%.([^.]+)$')
 --------------------------------------------
 
-function M.get_cword_num(cword)
-  vim.g.cword = cword
-  vim.g.cword_is_num = nil
+function M.get_str_num(str, hex)
+  vim.g.str = str
+  vim.g.hex = hex and 1 or 0
+  vim.g.str_is_num = nil
   vim.cmd [[
     python << EOF
+import re
 import vim
-cword = vim.eval('g:cword')
+str = vim.eval('g:str')
+hex = int(vim.eval('g:hex'))
+if not hex:
+  if len(str) > 2 and str[:2].lower() == '0x':
+    hex = 1
+  elif re.findall('[a-fA-F]', str):
+    hex = 1
 try:
-  if len(cword) > 2:
-    if cword[:2].lower() == '0x':
-      num = int(cword, 16)
-    else:
-      num = int(cword)
+  if hex:
+    num = int(str, 16)
   else:
-    num = int(cword)
-  vim.command(f'let g:cword_is_num = 1')
+    num = int(str)
+  vim.command(f'let g:str_is_num = 1')
   vim.command(f'let g:cnum = {num}')
 except Exception as e:
-  print('get_cword_num, e:', e)
+  e = ','.join(e.args).replace("'", '"')
+  vim.command(f"""lua require'my_base'.notify_error('get_str_num, error: {e}')""")
 EOF
 ]]
-  if vim.g.cword_is_num then
+  if vim.g.str_is_num then
     return vim.g.cnum
   end
   return nil
 end
 
 function M.count_bin(cword)
-  local cnum = M.get_cword_num(vim.fn.expand(cword))
+  local cnum = M.get_str_num(vim.fn.expand(cword))
   print(cnum)
 end
 
