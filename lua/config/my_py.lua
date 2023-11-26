0d9cf9a3ad20746d224f2d9e59065d2870c1d425
peter-lyr <llydrp.ldp@gmail.com>
2023-11-26 22:26:55 +0800

    runway start pause

--- lua/config/my_py.lua
+++ lua/config/my_py.lua
@@ -6,27 +6,35 @@ M.loaded = B.get_loaded(M.source)
 M.lua = string.match(M.loaded, '%.([^.]+)$')
 --------------------------------------------
 
-function M.run(way)
-  if not way then
-    way = 'asyncrun'
+function M.run(runway)
+  if not runway then
+    runway = 'asyncrun'
+  end
+  local pause = ''
+  if runway == 'start' then
+    pause = '& pause'
   end
   local cur_file = vim.api.nvim_buf_get_name(0)
   local fname = B.get_only_name(cur_file)
-  B.system_run(way, [[%s && python %s]], B.system_cd(cur_file), fname)
+  B.system_run(runway, [[%s && python %s %s]], B.system_cd(cur_file), fname, pause)
 end
 
-function M.toexe(way)
-  if not way then
-    way = 'asyncrun'
+function M.toexe(runway)
+  if not runway then
+    runway = 'asyncrun'
+  end
+  local pause = ''
+  if runway == 'start' then
+    pause = '& pause'
   end
   local cur_file = vim.api.nvim_buf_get_name(0)
   local fname = B.get_only_name(cur_file)
   local f = io.popen 'which pyinstaller'
   if f then
     if B.is(f:read '*a') then
-      B.system_run(way, [[%s && pyinstaller -F %s]], B.system_cd(cur_file), fname)
+      B.system_run(runway, [[%s && pyinstaller -F %s]], B.system_cd(cur_file), fname)
     else
-      B.system_run(way, [[pip install pyinstaller -i http://pypi.douban.com/simple --trusted-host pypi.douban.com && %s && pyinstaller -F %s]], B.system_cd(cur_file), fname)
+      B.system_run(runway, [[pip install pyinstaller -i http://pypi.douban.com/simple --trusted-host pypi.douban.com && %s && pyinstaller -F %s %s]], B.system_cd(cur_file), fname, pause)
     end
     f:close()
   end
