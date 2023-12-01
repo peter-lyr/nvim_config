78ce719b6007d06bc306f255ee985bdd7186242b
peter-lyr <llydrp.ldp@gmail.com>
2023-12-01 10:01:51 +0800

    system start cur file

--- lua/core/maps.lua
+++ lua/core/maps.lua
@@ -15,6 +15,7 @@ vim.keymap.set({ 'n', 'v', }, 'Q', 'q', { silent = true, desc = 'record', })
 vim.keymap.set({ 'n', 'v', }, 'c.', '<cmd>cd %:h<cr>', { silent = true, desc = 'c.', })
 
 -- start
+vim.keymap.set({ 'n', 'v', }, 'q<cr>', function() vim.fn.system('start /i cmd /c "' .. vim.api.nvim_buf_get_name(0) .. '"') end, { silent = true, desc = 'system start cur file', })
 vim.keymap.set({ 'n', 'v', }, 'q.', function() vim.fn.system('start ' .. vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':h')) end, { silent = true, desc = 'q.', })
 vim.keymap.set({ 'n', 'v', }, 'qw', function() vim.fn.system('start ' .. vim.loop.cwd()) end, { silent = true, desc = 'qw', })
 
